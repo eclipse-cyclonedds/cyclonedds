2b8274ea91e6a37e76d937992c1a61741249b271
Firas Sahli <firas.sahli@adlinktech.com>
2018-11-14 16:29:57 +0100

    Add a unit test for the function dds_time

---
+++ src/core/ddsc/tests/time.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <criterion/criterion.h>
+#include <criterion/logging.h>
+
+#include "ddsc/dds.h"
+#include "os/os.h"
+#include <stdlib.h>
+
+
+Test(dds_time, createTime, setup, teardown)
+{
+	dds_time_t resultTime;
+	resultTime= dds_time();
+
+	cr_assert_gt(resultTime, 0);
+}
