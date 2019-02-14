ab398b07803d823abfc8693d6dfaf68427df93a4
Erik Boasson <eb@ilities.com>
2019-02-14 10:45:42 +0100

    remove copyright claim on an otherwise unmodified file
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/os/src/snippets/code/os_stdlib_getopt.c
+++ src/os/src/snippets/code/os_stdlib_getopt.c
@@ -1,14 +1,3 @@
-/*
- * Copyright(c) 2006 to 2018 ADLINK Technology Limited and others
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
- * v. 1.0 which is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
- */
 /* ::[[ @(#) getopt.c 1.5 89/03/11 05:40:23 ]]:: */
 #ifndef LINT
 static const char Id[] = "$Id: os_stdlib_getopt.c,v 1.2 2008-11-17 09:53:45 hansv Exp $";
