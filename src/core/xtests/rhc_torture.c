3acabf23a53da52ab56bfc84e29b1892e4128d2b
Erik Boasson <eb@ilities.com>
2019-02-15 09:55:17 +0100

    address false positive from gcc in snprintf usage
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/xtests/rhc_torture.c
+++ src/core/xtests/rhc_torture.c
@@ -587,7 +587,7 @@ static void test_conditions (dds_entity_t pp, dds_entity_t tp, const int count, 
           if (conds[ci] <= 0) abort ();
           rhcconds[ci] = get_condaddr (conds[ci]);
           if (print) {
-            char buf[10];
+            char buf[18];
             snprintf (buf, sizeof (buf), "conds[%d]", ci);
             print_cond_w_addr (buf, conds[ci]);
           }
