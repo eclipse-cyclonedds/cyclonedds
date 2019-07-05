5a83d422eafa2595f0156d6045a2ac0e4c47faa1
Erik Boasson <eb@ilities.com>
2019-07-05 22:25:18 +0200

    Do not use msg_flags if DDSRT_MSGHDR_FLAGS not set
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/ddsi/src/ddsi_udp.c
+++ src/core/ddsi/src/ddsi_udp.c
@@ -147,7 +147,6 @@ static ssize_t ddsi_udp_conn_write (ddsi_tran_conn_t conn, const nn_locator_t *d
 #if DDSRT_MSGHDR_FLAGS
   msg.msg_flags = (int) flags;
 #else
-  msg.msg_flags = 0;
   DDSRT_UNUSED_ARG(flags);
 #endif
 #if MSG_NOSIGNAL && !LWIP_SOCKET
