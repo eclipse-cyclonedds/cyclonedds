9dd0c21f50e0c4b3f4bb8992834ff8daf7a43e23
Erik Boasson <eb@ilities.com>
2019-07-19 12:36:23 +0200

    Add multi-domain version of topic_data test
    This is the same test for verifying set_qos on topic_data is reflected
    in the DCPSPublication and DCPSSubscription topics as the one that
    already exists, but it uses two threads in two domains.  A barrier is
    used to ensure the threads in the two processes indeed execute
    concurrently, and different topic data sequences are used to increase
    the likelihood of detecting leakage from one domain into the other.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/mpt/tests/qos/ppuserdata.c
+++ src/mpt/tests/qos/ppuserdata.c
@@ -27,8 +27,8 @@ MPT_Test(qos, ppuserdata, .init=ppud_init, .fini=ppud_fini);
 /*
  * Checks whether reader/writer user_data QoS changes work.
  */
-#define TEST_A_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwuserdata", true, 10, RWUD_USERDATA)
-#define TEST_B_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwuserdata", false, 0, RWUD_USERDATA)
+#define TEST_A_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwuserdata", true, 10, RWUD_USERDATA, NULL)
+#define TEST_B_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwuserdata", false, 0, RWUD_USERDATA, NULL)
 MPT_TestProcess(qos, rwuserdata, a, rwud, TEST_A_ARGS);
 MPT_TestProcess(qos, rwuserdata, b, rwud, TEST_B_ARGS);
 MPT_Test(qos, rwuserdata, .init=ppud_init, .fini=ppud_fini);
@@ -38,8 +38,8 @@ MPT_Test(qos, rwuserdata, .init=ppud_init, .fini=ppud_fini);
 /*
  * Checks whether topic_data QoS changes become visible in reader/writer.
  */
-#define TEST_A_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwtopicdata", true, 10, RWUD_TOPICDATA)
-#define TEST_B_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwtopicdata", false, 0, RWUD_TOPICDATA)
+#define TEST_A_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwtopicdata", true, 10, RWUD_TOPICDATA, NULL)
+#define TEST_B_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwtopicdata", false, 0, RWUD_TOPICDATA, NULL)
 MPT_TestProcess(qos, rwtopicdata, a, rwud, TEST_A_ARGS);
 MPT_TestProcess(qos, rwtopicdata, b, rwud, TEST_B_ARGS);
 MPT_Test(qos, rwtopicdata, .init=ppud_init, .fini=ppud_fini);
@@ -49,10 +49,23 @@ MPT_Test(qos, rwtopicdata, .init=ppud_init, .fini=ppud_fini);
 /*
  * Checks whether group_data QoS changes become visible in reader/writer.
  */
-#define TEST_A_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwgroupdata", true, 10, RWUD_GROUPDATA)
-#define TEST_B_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwgroupdata", false, 0, RWUD_GROUPDATA)
+#define TEST_A_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwgroupdata", true, 10, RWUD_GROUPDATA, NULL)
+#define TEST_B_ARGS MPT_ArgValues(DDS_DOMAIN_DEFAULT, "rwgroupdata", false, 0, RWUD_GROUPDATA, NULL)
 MPT_TestProcess(qos, rwgroupdata, a, rwud, TEST_A_ARGS);
 MPT_TestProcess(qos, rwgroupdata, b, rwud, TEST_B_ARGS);
 MPT_Test(qos, rwgroupdata, .init=ppud_init, .fini=ppud_fini);
+#undef TEST_A_ARGS
+#undef TEST_B_ARGS
+
+/*
+ * Checks whether topic_data QoS changes become visible in reader/writer,
+ * but doing so in 2 domains simultaneously -- the specified domain id,
+ * and the one immediately above that
+ */
+#define TEST_A_ARGS MPT_ArgValues(3, "rwtopicdataM", true, 10, RWUD_TOPICDATA)
+#define TEST_B_ARGS MPT_ArgValues(3, "rwtopicdataM", false, 0, RWUD_TOPICDATA)
+MPT_TestProcess(qos, rwtopicdataM, a, rwudM, TEST_A_ARGS);
+MPT_TestProcess(qos, rwtopicdataM, b, rwudM, TEST_B_ARGS);
+MPT_Test(qos, rwtopicdataM, .init=ppud_init, .fini=ppud_fini);
 #undef TEST_A_ARGS
 #undef TEST_B_ARGS
