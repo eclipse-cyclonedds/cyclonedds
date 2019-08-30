5968fb92da82c29a523eb48001299489b9c1f933
Martin Bremmer <martin.bremmer@adlinktech.com>
2019-08-30 13:57:37 +0200

    Update windows dynlib.
    Signed-off-by: Martin Bremmer <martin.bremmer@adlinktech.com>

--- src/ddsrt/src/dynlib/windows/dynlib.c
+++ src/ddsrt/src/dynlib/windows/dynlib.c
@@ -17,79 +17,79 @@
 #include "dds/ddsrt/types.h"
 #include "dds/ddsrt/string.h"
 
-dds_retcode_t ddsrt_dlopen(const char *name, bool translate,
-		ddsrt_dynlib_t *handle) {
-	dds_retcode_t retcode = DDS_RETCODE_OK;
+dds_return_t ddsrt_dlopen(const char *name, bool translate,
+        ddsrt_dynlib_t *handle) {
+    dds_return_t retcode = DDS_RETCODE_OK;
 
-	assert( handle );
-	*handle = NULL;
+    assert( handle );
+    *handle = NULL;
 
-	if ((translate) && (strrchr(name, '/') == NULL )
-			&& (strrchr(name, '\\') == NULL )) {
-		/* Add suffix to the name and try to open. */
-		static const char suffix[] = ".dll";
-		size_t len = strlen(name) + sizeof(suffix);
-		char* libName = ddsrt_malloc(len);
-		sprintf_s(libName, len, "%s%s", name, suffix);
-		*handle = LoadLibrary(libName);
-		ddsrt_free(libName);
-	}
+    if ((translate) && (strrchr(name, '/') == NULL )
+            && (strrchr(name, '\\') == NULL )) {
+        /* Add suffix to the name and try to open. */
+        static const char suffix[] = ".dll";
+        size_t len = strlen(name) + sizeof(suffix);
+        char* libName = ddsrt_malloc(len);
+        sprintf_s(libName, len, "%s%s", name, suffix);
+        *handle = LoadLibrary(libName);
+        ddsrt_free(libName);
+    }
 
-	if (*handle == NULL) {
-		/* Name contains a path,
-		 * (auto)translate is disabled or
-		 * LoadLibrary on translated name failed. */
-		*handle = LoadLibrary(name);
-	}
+    if (*handle == NULL) {
+        /* Name contains a path,
+        * (auto)translate is disabled or
+        * LoadLibrary on translated name failed. */
+        *handle = LoadLibrary(name);
+    }
 
-	if (*handle != NULL) {
-		retcode = DDS_RETCODE_OK;
-	} else {
-		retcode = DDS_RETCODE_ERROR;
-	}
+    if (*handle != NULL) {
+        retcode = DDS_RETCODE_OK;
+    } else {
+        retcode = DDS_RETCODE_ERROR;
+    }
 
-	return retcode;
+    return retcode;
 }
 
-dds_retcode_t ddsrt_dlclose(ddsrt_dynlib_t handle) {
+dds_return_t ddsrt_dlclose(ddsrt_dynlib_t handle) {
 
-	assert ( handle );
-	return (FreeLibrary(handle) == 0) ? DDS_RETCODE_ERROR : DDS_RETCODE_OK;
+    assert ( handle );
+    return (FreeLibrary(handle) == 0) ? DDS_RETCODE_ERROR : DDS_RETCODE_OK;
 }
 
-dds_retcode_t ddsrt_dlsym(ddsrt_dynlib_t handle, const char *symbol,
-		void **address) {
-	dds_retcode_t retcode = DDS_RETCODE_OK;
+dds_return_t ddsrt_dlsym(ddsrt_dynlib_t handle, const char *symbol,
+        void **address) {
+    dds_return_t retcode = DDS_RETCODE_OK;
 
-	assert( handle );
-	assert( address );
-	assert( symbol );
+    assert( handle );
+    assert( address );
+    assert( symbol );
 
-	*address = GetProcAddress(handle, symbol);
-	if ( *address == NULL ) {
-		retcode = DDS_RETCODE_ERROR;
-	}
+    *address = GetProcAddress(handle, symbol);
+    if ( *address == NULL ) {
+        retcode = DDS_RETCODE_ERROR;
+    }
 
-	return retcode;
+    return retcode;
 }
 
-dds_retcode_t ddsrt_dlerror(char *buf, size_t buflen) {
+dds_return_t ddsrt_dlerror(char *buf, size_t buflen) {
 
-	/* Hopefully (and likely), the last error is
-	 * related to a Library action attempt. */
-	DWORD err;
-	assert ( buf );
+    /* Hopefully (and likely), the last error is
+    * related to a Library action attempt. */
+    DWORD err;
+    assert ( buf );
 
-	dds_retcode_t retcode = DDS_RETCODE_OK;
+    dds_return_t retcode = DDS_RETCODE_OK;
 
-	err = GetLastError();
-	if ( err == 0 ) {
-		retcode = DDS_RETCODE_NOT_FOUND;
-	} else {
-		retcode = ddsrt_strerror_r(err, buf, buflen);
-	}
+    err = GetLastError();
+    if ( err == 0 ) {
+        retcode = DDS_RETCODE_NOT_FOUND;
+    } else {
+        retcode = ddsrt_strerror_r(err, buf, buflen);
+    }
 
-	return retcode;
+    return retcode;
 
 }
 
