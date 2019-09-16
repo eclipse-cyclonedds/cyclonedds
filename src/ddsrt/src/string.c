d3f7faf691a649d220d3c4b8547bb4d3d1003d69
Jeroen Koekkoek <jeroen@koekkoek.nl>
2019-09-16 14:55:15 +0200

    Add support for #line directive
    * Process #line directives in the lexer and update the location structure
      accordingly; This allows the parser to generate correct syntax warnings when
      the preprocessor is hooked up eventually
    
    * Correct support for compiler directives. i.e. Fix the way #pragma directives
      are processed and document that compiler directives are not language
      constructs; This fix also makes it easier to hook up the preprocessor further
      down the road
    
    * Add ddsrt_strndup and test cases
    
    * Remove yy_decl.h and make use of %code provides block in idl.y instead
    
    * Replace ddsts_ prefix with idl_ in code that does not live in the ddsts
      component
    
    Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

--- src/ddsrt/src/string.c
+++ src/ddsrt/src/string.c
@@ -202,3 +202,21 @@ ddsrt_str_replace(
   memcpy(tmp, cur, lstr - (size_t) (cur - str) + 1);
   return res;
 }
+
+char *
+ddsrt_strndup(const char *str, size_t len)
+{
+  char *s;
+  size_t n = 0;
+
+  assert(str != NULL);
+
+  while (str[n] != '\0' && n < len) { n++; }
+
+  if ((s = ddsrt_malloc_s(n + 1)) != NULL) {
+    memcpy(s, str, n);
+    s[n] = '\0';
+  }
+
+  return s;
+}
