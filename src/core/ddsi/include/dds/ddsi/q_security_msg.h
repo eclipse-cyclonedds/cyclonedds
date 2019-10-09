dd67c718c8da69c78e8531c7aef46f54d33ff649
Martin Bremmer <martin.bremmer@adlinktech.com>
2019-10-09 10:05:07 +0200

    Added security Participant Generic Message.
    Signed-off-by: Martin Bremmer <martin.bremmer@adlinktech.com>

---
+++ src/core/ddsi/include/dds/ddsi/q_security_msg.h
@@ -0,0 +1,112 @@
+/*
+ * Copyright(c) 2006 to 2018 ADLINK Technology Limited and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+#ifndef Q_SECURITY_MSG_H
+#define Q_SECURITY_MSG_H
+
+#include "dds/ddsi/q_plist.h"
+#include "dds/ddsi/ddsi_guid.h"
+#include "dds/ddsrt/retcode.h"
+#include "dds/ddsi/ddsi_plist_generic.h"
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#define GMCLASSID_SECURITY_AUTH_REQUEST       "dds.sec.auth_request"
+#define GMCLASSID_SECURITY_AUTH_HANDSHAKE     "dds.sec.auth"
+
+typedef struct nn_message_identity {
+  ddsi_guid_t source_guid;
+  int64_t sequence_number;
+} nn_message_identity_t;
+
+typedef struct nn_participant_generic_message {
+  nn_message_identity_t message_identity;
+  nn_message_identity_t related_message_identity;
+  ddsi_guid_t destinaton_participant_guid;
+  ddsi_guid_t destination_endpoint_guid;
+  ddsi_guid_t source_endpoint_guid;
+  const char *message_class_id;
+  nn_dataholderseq_t message_data;
+} nn_participant_generic_message_t;
+
+
+/*
+ * The arguments are aliased in the resulting message structure.
+ * This means that the lifecycle of the arguments should be longer
+ * then that of the message.
+ */
+DDS_EXPORT void
+nn_participant_generic_message_init(
+   nn_participant_generic_message_t *msg,
+   const ddsi_guid_t *wrguid,
+   int64_t wrseq,
+   const ddsi_guid_t *dstpguid,
+   const ddsi_guid_t *dsteguid,
+   const ddsi_guid_t *srceguid,
+   const char *classid,
+   const nn_dataholderseq_t *mdata,
+   const nn_message_identity_t *rmid);
+
+/*
+ * Aliased struct variables will not be freed.
+ */
+DDS_EXPORT void
+nn_participant_generic_message_deinit(
+   nn_participant_generic_message_t *msg);
+
+/*
+ * The arguments are aliased in the resulting message structure.
+ */
+DDS_EXPORT nn_participant_generic_message_t *
+nn_participant_generic_message_new(
+   const ddsi_guid_t *wrguid,
+   int64_t wrseq,
+   const ddsi_guid_t *dstpguid,
+   const ddsi_guid_t *dsteguid,
+   const ddsi_guid_t *srceguid,
+   const char *classid,
+   const nn_dataholderseq_t *mdata,
+   const nn_message_identity_t *rmid);
+
+/*
+ * Aliased struct variables will not be freed.
+ */
+DDS_EXPORT void
+nn_participant_generic_message_free(
+   nn_participant_generic_message_t *msg);
+
+/*
+ * Some struct variables are aliased to the given buffer.
+ * This means that the lifecycle of the data buffer should be
+ * longer then that of the message.
+ */
+DDS_EXPORT dds_return_t
+nn_participant_generic_message_deseralize(
+   nn_participant_generic_message_t *msg,
+   const unsigned char *data,
+   size_t len,
+   bool bswap);
+
+DDS_EXPORT dds_return_t
+nn_participant_generic_message_serialize(
+   const nn_participant_generic_message_t *msg,
+   unsigned char **data,
+   size_t *len);
+
+DDS_EXPORT extern const enum pserop pserop_participant_generic_message[];
+
+#if defined (__cplusplus)
+}
+#endif
+
+#endif /* Q_SECURITY_MSG_H */
