17da91409a47d294949325fcb227daf2ac6535b6
Dan Rose <rotu@users.noreply.github.com>
2020-04-02 10:25:16 -0500

    Fix and simplify building docs and schemas (#441)
    Fix and simplify building docs and schemas
    
    1. Do an in-source build of derived schemas instead of building into build and checking that the files match.
    2. Fix paths of installed docs, which were failing on me
    3. Consolidate docs into same cmakelists that generates schema files
    4. Trang is usually available as an executable, so find it with find_program.
    5. Add doc dependencies to package.xml
    
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- docs/compare.pl
+++
@@ -1,8 +0,0 @@
-open A, "< $ARGV[0]" or die "can't open $ARGV[0]";
-open B, "< $ARGV[1]" or die "can't open $ARGV[1]";
-while (defined ($a = <A>) && defined ($b = <B>)) {
-  $a =~ s/[\r\n]+$//s;
-  $b =~ s/[\r\n]+$//s;
-  exit 1 unless $a eq $b;
-}
-exit 0;
