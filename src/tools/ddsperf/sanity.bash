fd3297d3e0d08cd64f4dd35de999afcd76e94261
Erik Boasson <eb@ilities.com>
2020-07-27 11:58:37 +0200

    Tweak sanity check script to reduce failures
    There is the occasional failure on Travis CI since 66daba9f, which
    appears to be caused by a legitimate increase in memory usage but not
    quite covered by the raised limits in that same commit.  This raises the
    limit further, but also lengthens the test to 20s to increase the
    likelihood of catching non-legitimate memory growth.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/tools/ddsperf/sanity.bash
+++ src/tools/ddsperf/sanity.bash
@@ -1,8 +1,8 @@
 exitcode=0
 # RSS/samples/roundtrip numbers are based on experimentation on Travis
-bin/ddsperf -L -D10 -n10 -Qminmatch:2 -Qrss:20% -Qrss:1 -Qsamples:300000 -Qroundtrips:3000 sub ping & ddsperf_pids=$!
-bin/ddsperf -L -D10 -n10 -Qminmatch:2 -Qrss:20% -Qrss:1 pub 100Hz burst 1000 & ddsperf_pids="$ddsperf_pids $!"
-sleep 11
+bin/ddsperf -L -D20 -n10 -Qminmatch:2 -Qrss:4 -Qsamples:300000 -Qroundtrips:3000 sub ping & ddsperf_pids=$!
+bin/ddsperf -L -D20 -n10 -Qminmatch:2 -Qrss:4 pub 100Hz burst 1000 & ddsperf_pids="$ddsperf_pids $!"
+sleep 21
 for pid in $ddsperf_pids ; do
     if kill -0 $pid 2>/dev/null ; then
         echo "killing process $pid"
