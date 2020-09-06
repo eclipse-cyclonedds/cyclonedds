869a95e3623fd4e8e3ceb772128fe414dfcfbae4
Jeroen Koekkoek <jeroen@koekkoek.nl>
2020-09-06 21:16:53 +0200

    Use llu as opposed to lu in node mask defines
    Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

--- src/idl/include/idl/tree.h
+++ src/idl/include/idl/tree.h
@@ -54,32 +54,32 @@ struct idl_location {
    on when applying a visitor pattern. */
 
 /** Member is (part of) the key */
-#define IDL_KEY (1lu<<35)                         /* IDL_PRAGMA / IDL_MEMBER */
+#define IDL_KEY (1llu<<35)                        /* IDL_PRAGMA / IDL_MEMBER */
 /** Identifier has been assigned (and set) using either @id or, if DDS-XTypes
     has been enabled, using @hashid. */
-#define IDL_ID (1lu<<34)                                       /* IDL_MEMBER */
+#define IDL_ID (1llu<<34)                                      /* IDL_MEMBER */
 /* pragmas */
-#define IDL_PRAGMA (1lu<<33)
-#define   IDL_KEYLIST (IDL_PRAGMA | 1lu)
-#define   IDL_DATA_TYPE (IDL_PRAGMA | 2lu)
+#define IDL_PRAGMA (1llu<<33)
+#define   IDL_KEYLIST (IDL_PRAGMA | 1llu)
+#define   IDL_DATA_TYPE (IDL_PRAGMA | 2llu)
 /* expressions */
-#define IDL_EXPR (1lu<<32)
-#define IDL_BINARY_EXPR (1lu<<31)                               /* IDL_EXPR */
-#define   IDL_OR_EXPR (IDL_BINARY_EXPR | 1lu)
-#define   IDL_XOR_EXPR (IDL_BINARY_EXPR | 2lu)
-#define   IDL_AND_EXPR (IDL_BINARY_EXPR | 3lu)
-#define   IDL_LSHIFT_EXPR (IDL_BINARY_EXPR | 4lu)
-#define   IDL_RSHIFT_EXPR (IDL_BINARY_EXPR | 5lu)
-#define   IDL_ADD_EXPR (IDL_BINARY_EXPR | 6lu)
-#define   IDL_SUB_EXPR (IDL_BINARY_EXPR | 7lu)
-#define   IDL_MULT_EXPR (IDL_BINARY_EXPR | 8lu)
-#define   IDL_DIV_EXPR (IDL_BINARY_EXPR | 9lu)
-#define   IDL_MOD_EXPR (IDL_BINARY_EXPR | 10lu)
-#define IDL_UNARY_EXPR (1lu<<30)                                /* IDL_EXPR */
-#define   IDL_MINUS_EXPR (IDL_UNARY_EXPR | 1lu)
-#define   IDL_PLUS_EXPR (IDL_UNARY_EXPR | 2lu)
-#define   IDL_NOT_EXPR (IDL_UNARY_EXPR | 3lu)
-#define IDL_LITERAL (1lu<<29)                                   /* IDL_EXPR */
+#define IDL_EXPR (1llu<<32)
+#define IDL_BINARY_EXPR (1llu<<31)                              /* IDL_EXPR */
+#define   IDL_OR_EXPR (IDL_BINARY_EXPR | 1llu)
+#define   IDL_XOR_EXPR (IDL_BINARY_EXPR | 2llu)
+#define   IDL_AND_EXPR (IDL_BINARY_EXPR | 3llu)
+#define   IDL_LSHIFT_EXPR (IDL_BINARY_EXPR | 4llu)
+#define   IDL_RSHIFT_EXPR (IDL_BINARY_EXPR | 5llu)
+#define   IDL_ADD_EXPR (IDL_BINARY_EXPR | 6llu)
+#define   IDL_SUB_EXPR (IDL_BINARY_EXPR | 7llu)
+#define   IDL_MULT_EXPR (IDL_BINARY_EXPR | 8llu)
+#define   IDL_DIV_EXPR (IDL_BINARY_EXPR | 9llu)
+#define   IDL_MOD_EXPR (IDL_BINARY_EXPR | 10llu)
+#define IDL_UNARY_EXPR (1llu<<30)                               /* IDL_EXPR */
+#define   IDL_MINUS_EXPR (IDL_UNARY_EXPR | 1llu)
+#define   IDL_PLUS_EXPR (IDL_UNARY_EXPR | 2llu)
+#define   IDL_NOT_EXPR (IDL_UNARY_EXPR | 3llu)
+#define IDL_LITERAL (1llu<<29)                                  /* IDL_EXPR */
 #define   IDL_INTEGER_LITERAL (IDL_LITERAL | IDL_ULLONG)
 #define   IDL_FLOATING_PT_LITERAL (IDL_LITERAL | IDL_LDOUBLE)
 #define   IDL_CHAR_LITERAL (IDL_LITERAL | IDL_CHAR)
@@ -88,60 +88,60 @@ struct idl_location {
 #define   IDL_STRING_LITERAL (IDL_LITERAL | IDL_STRING)
 #define   IDL_WSTRING_LITERAL (IDL_LITERAL | IDL_WSTRING)
 /* specifiers */
-#define IDL_TYPE (1lu<<28)
+#define IDL_TYPE (1llu<<28)
 /* declarations */
-#define IDL_DECL (1lu<<27)
-#define IDL_MODULE (1lu<<26)                                    /* IDL_DECL */
-#define IDL_CONST (1lu<<25)                            /* IDL_DECL / <type> */
-#define IDL_TYPEDEF (1lu<<24)                                   /* IDL_DECL */
-#define IDL_MEMBER (1lu<<23)                                    /* IDL_DECL */
-#define IDL_FORWARD (1lu<<22)                     /* IDL_STRUCT / IDL_UNION */
-#define IDL_CASE (1lu<<21)                                      /* IDL_DECL */
-#define IDL_CASE_LABEL (1lu<<20)                                /* IDL_DECL */
-#define IDL_ENUMERATOR (1lu<<19)                                /* IDL_DECL */
-#define IDL_DECLARATOR (1lu<<18)                                /* IDL_DECL */
-#define IDL_ANNOTATION_APPL (1lu<<17)                           /* IDL_DECL */
-#define IDL_ANNOTATION_APPL_PARAM (1lu<<16)                     /* IDL_DECL */
+#define IDL_DECL (1llu<<27)
+#define IDL_MODULE (1llu<<26)                                   /* IDL_DECL */
+#define IDL_CONST (1llu<<25)                           /* IDL_DECL / <type> */
+#define IDL_TYPEDEF (1llu<<24)                                  /* IDL_DECL */
+#define IDL_MEMBER (1llu<<23)                                   /* IDL_DECL */
+#define IDL_FORWARD (1llu<<22)                    /* IDL_STRUCT / IDL_UNION */
+#define IDL_CASE (1llu<<21)                                     /* IDL_DECL */
+#define IDL_CASE_LABEL (1llu<<20)                               /* IDL_DECL */
+#define IDL_ENUMERATOR (1llu<<19)                               /* IDL_DECL */
+#define IDL_DECLARATOR (1llu<<18)                               /* IDL_DECL */
+#define IDL_ANNOTATION_APPL (1llu<<17)                          /* IDL_DECL */
+#define IDL_ANNOTATION_APPL_PARAM (1llu<<16)                    /* IDL_DECL */
 /* constructed types */
-#define IDL_CONSTR_TYPE (1lu<<15)                    /* IDL_TYPE | IDL_DECL */
-#define IDL_STRUCT (1lu<<14)                             /* IDL_CONSTR_TYPE */
-#define IDL_UNION (1lu<<13)                              /* IDL_CONSTR_TYPE */
-#define IDL_ENUM (1lu<<12)                               /* IDL_CONSTR_TYPE */
+#define IDL_CONSTR_TYPE (1llu<<15)                   /* IDL_TYPE | IDL_DECL */
+#define IDL_STRUCT (1llu<<14)                            /* IDL_CONSTR_TYPE */
+#define IDL_UNION (1llu<<13)                             /* IDL_CONSTR_TYPE */
+#define IDL_ENUM (1llu<<12)                              /* IDL_CONSTR_TYPE */
 /* template types */
-#define IDL_TEMPL_TYPE (1lu<<11)
-#define   IDL_SEQUENCE (IDL_TEMPL_TYPE | 1lu)                   /* IDL_TYPE */
-#define   IDL_STRING (IDL_TEMPL_TYPE | 2lu)         /* IDL_CONST / IDL_TYPE */
-#define   IDL_WSTRING (IDL_TEMPL_TYPE | 3lu)        /* IDL_CONST / IDL_TYPE */
-#define   IDL_FIXED_PT (IDL_TEMPL_TYPE | 4lu)                   /* IDL_TYPE */
+#define IDL_TEMPL_TYPE (1llu<<11)
+#define   IDL_SEQUENCE (IDL_TEMPL_TYPE | 1llu)                  /* IDL_TYPE */
+#define   IDL_STRING (IDL_TEMPL_TYPE | 2llu)        /* IDL_CONST / IDL_TYPE */
+#define   IDL_WSTRING (IDL_TEMPL_TYPE | 3llu)       /* IDL_CONST / IDL_TYPE */
+#define   IDL_FIXED_PT (IDL_TEMPL_TYPE | 4llu)                  /* IDL_TYPE */
 /* simple types */
 /* miscellaneous base types */
-#define IDL_BASE_TYPE (1l<<10)                      /* IDL_CONST / IDL_TYPE */
-#define   IDL_CHAR (IDL_BASE_TYPE | 1lu)
-#define   IDL_WCHAR (IDL_BASE_TYPE | 2lu)
-#define   IDL_BOOL (IDL_BASE_TYPE | 3lu)
-#define   IDL_OCTET (IDL_BASE_TYPE | 4lu)
+#define IDL_BASE_TYPE (1llu<<10)                    /* IDL_CONST / IDL_TYPE */
+#define   IDL_CHAR (IDL_BASE_TYPE | 1llu)
+#define   IDL_WCHAR (IDL_BASE_TYPE | 2llu)
+#define   IDL_BOOL (IDL_BASE_TYPE | 3llu)
+#define   IDL_OCTET (IDL_BASE_TYPE | 4llu)
 /* integer types */
-#define IDL_INTEGER_TYPE (IDL_BASE_TYPE | 1lu<<9)          /* IDL_BASE_TYPE */
-#define IDL_UNSIGNED (1lu<<0)
-#define   IDL_INT8 (IDL_INTEGER_TYPE | (1lu<<1))
+#define IDL_INTEGER_TYPE (IDL_BASE_TYPE | 1llu<<9)         /* IDL_BASE_TYPE */
+#define IDL_UNSIGNED (1llu<<0)
+#define   IDL_INT8 (IDL_INTEGER_TYPE | (1llu<<1))
 #define   IDL_UINT8 (IDL_INT8 | IDL_UNSIGNED)
-#define   IDL_INT16 (IDL_INTEGER_TYPE | (2lu<<1))
+#define   IDL_INT16 (IDL_INTEGER_TYPE | (2llu<<1))
 #define   IDL_UINT16 (IDL_INT16 | IDL_UNSIGNED)
 #define   IDL_SHORT (IDL_INT16)
 #define   IDL_USHORT (IDL_UINT16)
-#define   IDL_INT32 (IDL_INTEGER_TYPE | (3lu<<1))
+#define   IDL_INT32 (IDL_INTEGER_TYPE | (3llu<<1))
 #define   IDL_UINT32 (IDL_INT32 | IDL_UNSIGNED)
 #define   IDL_LONG (IDL_INT32)
 #define   IDL_ULONG (IDL_UINT32)
-#define   IDL_INT64 (IDL_INTEGER_TYPE | (4lu<<1))
+#define   IDL_INT64 (IDL_INTEGER_TYPE | (4llu<<1))
 #define   IDL_UINT64 (IDL_INT64 | IDL_UNSIGNED)
 #define   IDL_LLONG (IDL_INT64)
 #define   IDL_ULLONG (IDL_UINT64)
 /* floating point types */
-#define IDL_FLOATING_PT_TYPE (IDL_BASE_TYPE | (1lu<<8))    /* IDL_BASE_TYPE */
-#define   IDL_FLOAT (IDL_FLOATING_PT_TYPE | 1lu)
-#define   IDL_DOUBLE (IDL_FLOATING_PT_TYPE | 2lu)
-#define   IDL_LDOUBLE (IDL_FLOATING_PT_TYPE | 3lu)
+#define IDL_FLOATING_PT_TYPE (IDL_BASE_TYPE | (1llu<<8))   /* IDL_BASE_TYPE */
+#define   IDL_FLOAT (IDL_FLOATING_PT_TYPE | 1llu)
+#define   IDL_DOUBLE (IDL_FLOATING_PT_TYPE | 2llu)
+#define   IDL_LDOUBLE (IDL_FLOATING_PT_TYPE | 3llu)
 
 typedef void(*idl_print_t)(const void *node);
 typedef void(*idl_delete_t)(void *node);
