aefe2d8e565b32c14b6c6312d848b6c73d9d96e7
Erik Boasson <eb@ilities.com>
2020-09-07 11:01:13 +0200

    Remove unnecessary flags from security timed cb
    The xevents "callback" type events are guaranteed to have ceased
    executing on return from delete_xevent_callback, and so the tests for
    "busy" are not needed.  That also eliminates the need for the condition
    variable.
    
    The "active" flag can be replaced by checking whether the event
    pointer is null or not.
    
    Otherwise a typo and some indentation fixes.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/security/core/src/dds_security_timed_cb.c
+++ src/security/core/src/dds_security_timed_cb.c
@@ -29,10 +29,7 @@
 
 struct dds_security_timed_dispatcher
 {
-  bool active;
-  bool busy;
   ddsrt_mutex_t lock;
-  ddsrt_cond_t cond;
   struct xevent *evt;
   ddsrt_avl_tree_t events;
   ddsrt_fibheap_t timers;
@@ -49,25 +46,24 @@ struct dds_security_timed_event
   void *arg;
 };
 
-static int compare_time_evemt (const void *va, const void *vb);
+static int compare_time_event (const void *va, const void *vb);
 static int compare_timed_cb_trigger_time(const void *va, const void *vb);
 
-static const ddsrt_avl_treedef_t timed_event_treedef = DDSRT_AVL_TREEDEF_INITIALIZER (offsetof (struct dds_security_timed_event, avlnode), offsetof (struct dds_security_timed_event, handle), compare_time_evemt, 0);
+static const ddsrt_avl_treedef_t timed_event_treedef = DDSRT_AVL_TREEDEF_INITIALIZER (offsetof (struct dds_security_timed_event, avlnode), offsetof (struct dds_security_timed_event, handle), compare_time_event, 0);
 static const ddsrt_fibheap_def_t timed_cb_queue_fhdef = DDSRT_FIBHEAPDEF_INITIALIZER(offsetof(struct dds_security_timed_event, heapnode), compare_timed_cb_trigger_time);
 
-static int compare_time_evemt (const void *va, const void *vb)
+static int compare_time_event (const void *va, const void *vb)
 {
   const dds_security_time_event_handle_t *ha = va;
   const dds_security_time_event_handle_t *hb = vb;
-
   return ((*ha > *hb) ? 1 : (*ha < *hb) ?  -1 : 0);
 }
 
 static int compare_timed_cb_trigger_time(const void *va, const void *vb)
 {
-    const struct dds_security_timed_event *a = va;
-    const struct dds_security_timed_event *b = vb;
-    return (a->trigger_time == b->trigger_time) ? 0 : (a->trigger_time < b->trigger_time) ? -1 : 1;
+  const struct dds_security_timed_event *a = va;
+  const struct dds_security_timed_event *b = vb;
+  return (a->trigger_time == b->trigger_time) ? 0 : (a->trigger_time < b->trigger_time) ? -1 : 1;
 }
 
 static void timed_event_cb (struct xevent *xev, void *varg, ddsrt_mtime_t tnow);
@@ -94,12 +90,6 @@ static void timed_event_cb (struct xevent *xev, void *arg, ddsrt_mtime_t tnow)
 
   ddsrt_mutex_lock(&dispatcher->lock);
 
-  if (!dispatcher->active)
-  {
-    ddsrt_mutex_unlock(&dispatcher->lock);
-    return;
-  }
-
   do {
     if ((ev = ddsrt_fibheap_min(&timed_cb_queue_fhdef, &dispatcher->timers)) != NULL)
     {
@@ -110,12 +100,9 @@ static void timed_event_cb (struct xevent *xev, void *arg, ddsrt_mtime_t tnow)
       {
         (void)ddsrt_fibheap_extract_min(&timed_cb_queue_fhdef, &dispatcher->timers);
         ddsrt_avl_delete(&timed_event_treedef, &dispatcher->events, ev);
-        dispatcher->busy = true;
         ddsrt_mutex_unlock(&dispatcher->lock);
         ev->callback(ev->handle, ev->trigger_time, DDS_SECURITY_TIMED_CB_KIND_TIMEOUT, ev->arg);
         ddsrt_mutex_lock(&dispatcher->lock);
-        dispatcher->busy = false;
-        ddsrt_cond_signal(&dispatcher->cond);
         ddsrt_free(ev);
       }
     }
@@ -132,12 +119,8 @@ static void timed_event_cb (struct xevent *xev, void *arg, ddsrt_mtime_t tnow)
 
 struct dds_security_timed_dispatcher * dds_security_timed_dispatcher_new(void)
 {
-	struct dds_security_timed_dispatcher *d = ddsrt_malloc(sizeof(*d));
-
-	d->active = false;
-	d->busy = false;
-	ddsrt_mutex_init (&d->lock);
-	ddsrt_cond_init(&d->cond);
+  struct dds_security_timed_dispatcher *d = ddsrt_malloc(sizeof(*d));
+  ddsrt_mutex_init (&d->lock);
   ddsrt_avl_init(&timed_event_treedef, &d->events);
   ddsrt_fibheap_init(&timed_cb_queue_fhdef, &d->timers);
   d->evt = NULL;
@@ -148,7 +131,7 @@ struct dds_security_timed_dispatcher * dds_security_timed_dispatcher_new(void)
 void dds_security_timed_dispatcher_enable(struct dds_security_timed_dispatcher *d, struct xeventq *evq)
 {
   ddsrt_mutex_lock(&d->lock);
-  if (!d->active)
+  if (d->evt == NULL)
   {
     struct dds_security_timed_event *ev = ddsrt_fibheap_min(&timed_cb_queue_fhdef, &d->timers);
     ddsrt_mtime_t tsched = DDSRT_MTIME_NEVER;
@@ -161,7 +144,6 @@ void dds_security_timed_dispatcher_enable(struct dds_security_timed_dispatcher *
       tsched = ddsrt_mtime_add_duration(ddsrt_time_monotonic(), delta < CHECK_TIMER_INTERVAL ? delta : CHECK_TIMER_INTERVAL);
     }
     d->evt = qxev_callback (evq, tsched, timed_event_cb, d);
-    d->active = true;
   }
   ddsrt_mutex_unlock(&d->lock);
 }
@@ -169,13 +151,10 @@ void dds_security_timed_dispatcher_enable(struct dds_security_timed_dispatcher *
 void dds_security_timed_dispatcher_disable(struct dds_security_timed_dispatcher *d)
 {
   ddsrt_mutex_lock(&d->lock);
-  if (d->active)
+  if (d->evt != NULL)
   {
-    d->active = false;
     delete_xevent_callback(d->evt);
     d->evt = NULL;
-    while (d->busy)
-      (void)ddsrt_cond_wait(&d->cond, &d->lock);
   }
   ddsrt_mutex_unlock(&d->lock);
 }
@@ -192,7 +171,6 @@ void dds_security_timed_dispatcher_free(struct dds_security_timed_dispatcher *d)
     ddsrt_free(ev);
     ev = ddsrt_fibheap_extract_min(&timed_cb_queue_fhdef, &d->timers);
   }
-  ddsrt_cond_destroy(&d->cond);
   ddsrt_mutex_destroy(&d->lock);
   ddsrt_free(d);
 }
@@ -209,7 +187,7 @@ dds_security_time_event_handle_t dds_security_timed_dispatcher_add(struct dds_se
   ddsrt_avl_insert(&timed_event_treedef, &d->events, ev);
   ddsrt_fibheap_insert(&timed_cb_queue_fhdef, &d->timers, ev);
   d->next_timer++;
-  if (d->active)
+  if (d->evt != NULL)
   {
     ddsrt_mtime_t tsched = ddsrt_mtime_add_duration(ddsrt_time_monotonic(), delta < CHECK_TIMER_INTERVAL ? delta : CHECK_TIMER_INTERVAL);
     (void)resched_xevent_if_earlier(d->evt, tsched);
@@ -240,5 +218,4 @@ void dds_security_timed_dispatcher_remove(struct dds_security_timed_dispatcher *
     ev->callback(ev->handle, ev->trigger_time, DDS_SECURITY_TIMED_CB_KIND_DELETE, ev->arg);
     ddsrt_free(ev);
   }
-
 }
