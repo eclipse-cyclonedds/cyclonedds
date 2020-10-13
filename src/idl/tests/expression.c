cb6f4289d86e380375419c73ea36a894ac53a799
Jeroen Koekkoek <jeroen@koekkoek.nl>
2020-10-13 14:41:57 +0200

    Add support for const string declarations
    Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

--- src/idl/tests/expression.c
+++ src/idl/tests/expression.c
@@ -107,3 +107,24 @@ CU_Test(idl_expression, unary_not_minus)
   expr2.right = (idl_const_expr_t *)&lit;
   test_unary_expr(&expr1, IDL_RETCODE_OK, &var);
 }
+
+CU_Test(idl_expression, const_string)
+{
+  idl_retcode_t ret;
+  idl_tree_t *tree = NULL;
+  idl_const_t *c;
+  idl_constval_t *v;
+
+  const char idl[] = "const string<12> c = \"foo\" \"bar\";";
+
+  ret = idl_parse_string(idl, 0u, &tree);
+  CU_ASSERT_EQUAL_FATAL(ret, IDL_RETCODE_OK);
+  CU_ASSERT_PTR_NOT_NULL_FATAL(tree);
+  c = (idl_const_t *)tree->root;
+  CU_ASSERT_FATAL(idl_is_const(c));
+  v = (idl_constval_t *)c->const_expr;
+  CU_ASSERT_FATAL(idl_is_masked(v, IDL_CONST|IDL_STRING));
+  CU_ASSERT_PTR_NOT_NULL(v->value.str);
+  CU_ASSERT_STRING_EQUAL(v->value.str, "foobar");
+  idl_delete_tree(tree);
+}
