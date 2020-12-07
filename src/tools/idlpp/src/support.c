32d5efc9081802848c323320ba3a67b8509b1546
Jeroen Koekkoek <jeroen@koekkoek.nl>
2020-12-07 14:32:59 +0100

    Fix output of line comments in mcpp
    Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

--- src/tools/idlpp/src/support.c
+++ src/tools/idlpp/src/support.c
@@ -183,7 +183,7 @@ static char *   append_to_buffer(
     size_t      length
 )
 {
-    if (mem_buf_p->bytes_avail < length) {  /* Need to allocate more memory */
+    if (mem_buf_p->bytes_avail < length + 1) {  /* Need to allocate more memory */
         size_t size = MAX( BUF_INCR_SIZE, length);
 
         if (mem_buf_p->buffer == NULL) {            /* 1st append   */
@@ -1718,6 +1718,8 @@ static char *   parse_line( void)
                     sp -= 2;
                     while (*sp != '\n')     /* Until end of line    */
                         mcpp_fputc( *sp++, OUT);
+                    mcpp_fputc( '\n', OUT);
+                    wrong_line = TRUE;
                 }
                 goto  end_line;
             default:                        /* Not a comment        */
