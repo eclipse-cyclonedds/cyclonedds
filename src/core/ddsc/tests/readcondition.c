b823b3ce4a7e300140e8ec1b5e0d0606338d96ac
Erik Boasson <eb@ilities.com>
2021-01-10 10:00:27 +0100

    Test delete readcondition while triggering
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/ddsc/tests/readcondition.c
+++ src/core/ddsc/tests/readcondition.c
@@ -11,8 +11,10 @@
  */
 #include "dds/dds.h"
 #include "dds/ddsrt/misc.h"
+#include "dds/ddsrt/atomics.h"
 #include "dds/ddsrt/process.h"
 #include "dds/ddsrt/threads.h"
+#include "dds/ddsrt/random.h"
 
 #include "test_common.h"
 
@@ -1452,6 +1454,113 @@ CU_Test(ddsc_readcondition_take, already_deleted, .init=readcondition_init, .fin
 }
 /*************************************************************************************************/
 
-
-
 #endif
+
+struct writethread_arg {
+  dds_entity_t wr;
+  ddsrt_atomic_uint32_t stop;
+};
+
+static uint32_t writethread (void *varg)
+{
+  struct writethread_arg * const arg = varg;
+  Space_Type1 data = { 0, 0, 0 };
+  dds_return_t ret = 0;
+  while (!ddsrt_atomic_ld32 (&arg->stop) && ret == 0)
+  {
+    data.long_3++;
+    ret = dds_write (arg->wr, &data);
+  }
+  ddsrt_atomic_or32 (&arg->stop, (ret != 0) ? 2 : 0);
+  printf ("nwrites: %d\n", (int) data.long_3);
+  return 0;
+}
+
+CU_Test(ddsc_readcondition, stress)
+{
+#define NCONDS 10
+  const dds_duration_t duration = DDS_SECS (3);
+  dds_return_t rc;
+
+  const dds_entity_t pp = dds_create_participant (0, NULL, NULL);
+  CU_ASSERT_FATAL (pp > 0);
+
+  char tpname[100];
+  create_unique_topic_name ("ddsc_data_avail_stress_delete_reader", tpname, sizeof (tpname));
+
+  dds_qos_t * const qos = dds_create_qos ();
+  CU_ASSERT_FATAL (qos != NULL);
+  dds_qset_reliability (qos, DDS_RELIABILITY_RELIABLE, DDS_SECS (1));
+  dds_qset_writer_data_lifecycle (qos, false);
+  const dds_entity_t tp = dds_create_topic (pp, &Space_Type1_desc, tpname, qos, NULL);
+  CU_ASSERT_FATAL (tp > 0);
+  dds_delete_qos (qos);
+
+  const dds_entity_t wr = dds_create_writer (pp, tp, NULL, NULL);
+  CU_ASSERT_FATAL (wr > 0);
+  struct writethread_arg wrarg = {
+    .wr = wr,
+    .stop = DDSRT_ATOMIC_UINT32_INIT (0)
+  };
+  ddsrt_threadattr_t tattr;
+  ddsrt_thread_t wrtid;
+  ddsrt_threadattr_init (&tattr);
+  rc = ddsrt_thread_create (&wrtid, "writer", &tattr, writethread, &wrarg);
+  CU_ASSERT_FATAL (rc == 0);
+
+  const dds_entity_t rd = dds_create_reader (pp, tp, NULL, NULL);
+  CU_ASSERT_FATAL (rd > 0);
+  const dds_entity_t ws = dds_create_waitset (pp);
+  CU_ASSERT_FATAL (ws > 0);
+
+  const dds_time_t tend = dds_time () + duration;
+  uint32_t nconds = 0;
+  dds_entity_t conds[NCONDS] = { 0 };
+  while (!ddsrt_atomic_ld32 (&wrarg.stop) && dds_time () < tend)
+  {
+    const uint32_t condidx = ddsrt_random () % NCONDS;
+    if (conds[condidx])
+    {
+      rc = dds_delete (conds[condidx]);
+      CU_ASSERT_FATAL (rc == 0);
+      conds[condidx] = 0;
+    }
+
+    conds[condidx] = dds_create_readcondition (rd, DDS_ANY_STATE);
+    CU_ASSERT_FATAL (conds[condidx] > 0);
+    
+    // the fact that read conditions get updated even when not attached to a waitset is
+    // probably a bug, so let's attach it to a waitset for good measure
+    rc = dds_waitset_attach (ws, conds[condidx], conds[condidx]);
+    CU_ASSERT_FATAL (rc == 0);
+
+    // take whatever sample happens to be present: we want the read condition to be triggered
+    // by the _arrival_ of a sample
+    {
+      Space_Type1 sample;
+      void *sampleptr = &sample;
+      dds_sample_info_t si;
+      rc = dds_take (rd, &sampleptr, &si, 1, 1);
+      CU_ASSERT_FATAL (rc == 0 || rc == 1);
+    }
+
+    while (!ddsrt_atomic_ld32 (&wrarg.stop) && dds_time () < tend && !dds_triggered (conds[condidx]))
+    {
+      // spin to maximize number of conditions created/deleted
+    }
+
+    nconds++;
+  }
+  ddsrt_atomic_or32 (&wrarg.stop, 1);
+  ddsrt_thread_join (wrtid, NULL);
+
+  printf ("nconds %"PRIu32"\n", nconds);
+  printf ("stop %"PRIu32"\n", ddsrt_atomic_ld32 (&wrarg.stop));
+
+  CU_ASSERT_FATAL (nconds > 100); // sanity check
+  CU_ASSERT_FATAL (!(ddsrt_atomic_ld32 (&wrarg.stop) & 2));
+
+  rc = dds_delete (pp);
+  CU_ASSERT_FATAL (rc == 0);
+#undef NCONDS
+}
