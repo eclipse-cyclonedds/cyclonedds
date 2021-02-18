5c194d95dc365d6b47e0a3026efe6b08d805c96c
Erik Boasson <eb@ilities.com>
2021-02-18 10:43:41 +0100

    Serialize updates to concurrent hopscotch hash
    There are intermittent failures on Travis CI where tests seem to
    randomly timeout and appear to do so at the end of the test.  Sometimes
    this happens on a machine and in a state where the liveliness monitoring
    will cough up a stack trace.  In the cases where I have seen such stack
    traces and they contain more than mere numbers (so at least some symbol
    names), there was often (perhaps always, I am not sure of that) one
    thread attempting to lock a bucket in the concurrent hash table and
    being blocked in a ddsrt_cond_wait.  One can only conclude that there
    may be a problem in that implementation.
    
    This commit eliminates the concurrent updates while leaving the lookups
    lock-free.  The locking scheme becomes a simple mutex around the
    add/remove operations.  With this change I have not seen any sign of
    these particular timeouts in a fairly large number of CI runs.  I
    confess to not yet having been able to reproduce it on either macOS or
    Linux, x64 or ARM, and to not yet knowing the root cause, but I think
    that the circumstantial evidence by now is enough to make this change.
    
    It so happens that this change is unlikely to be bad for performance.
    The argument is twofold: (1) contention on updates is not actually an
    issue given how the hash tables are used; and (2) the simpler locking
    scheme allows for more efficient use of memory and should on average
    yield a small reduction in cache misses on lookups.
    
    Re (1): there are two uses of the concurrent hash table in Cyclone: the
    GUID index (ephash) and the key-to-instance-id map (tkmap).  The former
    is changed only when entities are created or deleted, which is a fairly
    expensive operation and executed relatively rarely.  Contention on this
    hash table for updates is not really not an issue.
    
    For the key-to-instance-id map, in DDS instances/key values are
    relatively heavy-weight because of the registrations and the need to
    unregister after writing.  Consequently, the vast majority of the data
    flowing through the system reuses existing key values.  The
    implementation is such that it only tries to update the hash table after
    a lookup has failed.  Again leading to the conclusion that contention
    on updates should not be a significant issue.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/ddsrt/tests/hopscotch.c
+++ src/ddsrt/tests/hopscotch.c
@@ -20,6 +20,8 @@
 #include "dds/ddsrt/heap.h"
 #include "dds/ddsrt/time.h"
 #include "dds/ddsrt/hopscotch.h"
+#include "dds/ddsrt/atomics.h"
+#include "dds/ddsrt/threads.h"
 
 #define MAX_NKEYS 10000
 #define MAX_ITERS 1000000
@@ -58,7 +60,14 @@ static void swap (uint32_t *a, uint32_t *b)
 static void init (bool random)
 {
   uint32_t i;
-  ddsrt_prng_init_simple (&prng, ddsrt_random ());
+
+  ddsrt_prng_seed_t prng_seed;
+  bool haveseed = ddsrt_prng_makeseed (&prng_seed);
+  CU_ASSERT_FATAL (haveseed);
+  ddsrt_prng_init (&prng, &prng_seed);
+  printf ("%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32"\n",
+          prng_seed.key[0], prng_seed.key[1], prng_seed.key[2], prng_seed.key[3], prng_seed.key[4], prng_seed.key[5], prng_seed.key[6], prng_seed.key[7]);
+
   next_v = MAX_NKEYS;
   for (i = 0; i < MAX_NKEYS; i++)
   {
@@ -159,15 +168,17 @@ CU_TheoryDataPoints (ddsrt_hopscotch, random) = {
   CU_DataPoints (const char *,       "nop",   "nop",   "nop",    "seq",   "seq",   "seq")
 };
 
-CU_Theory ((const struct ops *ops, bool random, adj_fun_t adj, const char *adjname), ddsrt_hopscotch, random)
+CU_Theory ((const struct ops *ops, bool random, adj_fun_t adj, const char *adjname), ddsrt_hopscotch, random, .timeout = 20)
 {
-  printf ("%s random=%d adj=%s", ops->name, random, adjname);
+  printf ("%"PRId64" %s random=%d adj=%s\n", ddsrt_time_monotonic().v, ops->name, random, adjname);
   fflush (stdout);
   init (random);
   void *h = ops->new ();
   uint32_t i, nk = 0;
   uint64_t nn = 0;
   ddsrt_mtime_t t0, t1;
+  printf ("%"PRId64" %s start\n", ddsrt_time_monotonic().v, ops->name);
+  fflush (stdout);
   t0 = ddsrt_time_monotonic ();
   for (uint32_t iter = 0; iter < MAX_ITERS; iter++)
   {
@@ -205,5 +216,192 @@ CU_Theory ((const struct ops *ops, bool random, adj_fun_t adj, const char *adjna
   }
   t1 = ddsrt_time_monotonic ();
   ops->free (h);
-  printf (" %"PRIu64" %.0f ns/cycle\n", nn, (double) (t1.v - t0.v) / (double) nn);
+  printf ("%"PRId64" %s done %"PRIu64" %.0f ns/cycle\n", ddsrt_time_monotonic().v, ops->name, nn, (double) (t1.v - t0.v) / (double) nn);
+  fflush (stdout);
+}
+
+struct gcelem {
+  struct gcelem *next;
+};
+
+static void chhtest_gc (void *block, void *arg)
+{
+  // block is large enough
+  // simply defer freeing memory until the end of the test
+  struct gcelem **gclist = arg;
+  struct gcelem *elem = block;
+  elem->next = *gclist;
+  *gclist = elem;
+}
+
+struct chhtest_thread_arg {
+  ddsrt_atomic_uint32_t *stop;
+  struct ddsrt_chh *chh;
+  uint32_t nkeys;
+  uint32_t *keys;
+  bool check;
+
+  uint32_t adds, removes, lookups, maxnkeys;
+};
+
+static uint32_t chhtest_thread (void *varg)
+{
+  struct chhtest_thread_arg * const __restrict arg = varg;
+  uint32_t ** ksptrs;
+  uint32_t n = 0;
+
+  ksptrs = ddsrt_malloc (arg->nkeys * sizeof (*ksptrs));
+  for (uint32_t i = 0; i < arg->nkeys; i++)
+    ksptrs[i] = &arg->keys[i];
+
+  arg->adds = arg->removes = arg->lookups = arg->maxnkeys = 0;
+
+  ddsrt_prng_t local_prng;
+  ddsrt_prng_seed_t prng_seed;
+  bool haveseed = ddsrt_prng_makeseed (&prng_seed);
+  CU_ASSERT_FATAL (haveseed);
+  ddsrt_prng_init (&local_prng, &prng_seed);
+  printf ("%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32".%08"PRIx32"\n",
+          prng_seed.key[0], prng_seed.key[1], prng_seed.key[2], prng_seed.key[3], prng_seed.key[4], prng_seed.key[5], prng_seed.key[6], prng_seed.key[7]);
+  while (!ddsrt_atomic_ld32 (arg->stop))
+  {
+    const uint32_t raw_oper = ddsrt_prng_random (&local_prng);
+    switch (raw_oper % 4)
+    {
+      case 0: /* add */
+        if (n < arg->nkeys)
+        {
+          bool x = ddsrt_chh_add (arg->chh, ksptrs[n]);
+          if (arg->check && !x) { CU_ASSERT_FATAL (0); }
+          n++;
+          arg->adds++;
+          if (n > arg->maxnkeys)
+            arg->maxnkeys = n;
+        }
+        break;
+      case 1: /* remove */
+        if (n > 0)
+        {
+          const uint32_t ix = (raw_oper >> 2) % n;
+          uint32_t * const obj = ksptrs[ix];
+          bool x = ddsrt_chh_remove (arg->chh, obj);
+          if (arg->check && !x) { CU_ASSERT_FATAL (0); }
+          ksptrs[ix] = ksptrs[--n];
+          ksptrs[n] = obj;
+          arg->removes++;
+        }
+        break;
+      case 2: case 3: /* lookup */
+        {
+          const uint32_t ix = (raw_oper >> 2) % arg->nkeys;
+          bool x = ddsrt_chh_lookup (arg->chh, ksptrs[ix]);
+          if (arg->check && ((ix < n) ? !x : x)) { CU_ASSERT_FATAL (0) };
+          arg->lookups++;
+        }
+        break;
+    }
+  }
+
+  // "erase" the entries not in the hash table to simplify checking
+  // after all threads have stopped
+  for (uint32_t i = n; i < arg->nkeys; i++)
+    *ksptrs[i] = UINT32_MAX;
+  ddsrt_free (ksptrs);
+  return 0;
+}
+
+static void chhtest_check (void *vobj, void *varg)
+{
+  uint32_t *obj = vobj;
+  uint32_t *count = varg;
+  CU_ASSERT_FATAL (*obj != UINT32_MAX);
+  (*count)++;
+}
+
+static void chhtest_count (void *vobj, void *varg)
+{
+  uint32_t *count = varg;
+  (void) vobj;
+  (*count)++;
+}
+
+CU_Test(ddsrt_hopscotch, concurrent, .timeout = 20)
+{
+  const uint32_t nkeys = 100;
+  const bool check = false;
+  struct ddsrt_chh *chh;
+  struct gcelem *gclist = NULL;
+  uint32_t *keyset = ddsrt_malloc (nkeys * sizeof (*keyset));
+  for (uint32_t i = 0; i < nkeys; i++)
+    keyset[i] = i;
+
+  chh = ddsrt_chh_new (1, hash_uint32, equals_uint32, chhtest_gc, &gclist);
+  CU_ASSERT_FATAL (chh != NULL);
+
+  ddsrt_atomic_uint32_t stop = DDSRT_ATOMIC_UINT32_INIT (0);
+  struct chhtest_thread_arg args[4];
+  ddsrt_thread_t tids[4];
+  for (uint32_t i = 0; i < 4; i++)
+  {
+    args[i].chh = chh;
+    args[i].stop = &stop;
+    if (check)
+    {
+      args[i].keys = keyset + i * (nkeys / 4);
+      args[i].nkeys = nkeys / 4;
+    }
+    else
+    {
+      args[i].keys = keyset;
+      args[i].nkeys = nkeys;
+    }
+    args[i].check = check;
+  }
+  for (uint32_t i = 0; i < 4; i++)
+  {
+    ddsrt_threadattr_t attr;
+    ddsrt_threadattr_init (&attr);
+    dds_return_t ret = ddsrt_thread_create (&tids[i], "x", &attr, chhtest_thread, &args[i]);
+    CU_ASSERT_FATAL (ret == 0);
+  }
+
+  dds_time_t tend = dds_time () + DDS_SECS (15);
+  while (dds_time () < tend)
+    dds_sleepfor (DDS_MSECS (100));
+  ddsrt_atomic_st32 (&stop, 1);
+  ddsrt_atomic_fence ();
+
+  for (uint32_t i = 0; i < 4; i++)
+  {
+    dds_return_t ret = ddsrt_thread_join (tids[i], NULL);
+    CU_ASSERT_FATAL (ret == 0);
+    printf ("args[%"PRIu32"] add %"PRIu32" rm %"PRIu32" lk %"PRIu32" max %"PRIu32"\n",
+            i, args[i].adds, args[i].removes, args[i].lookups, args[i].maxnkeys);
+  }
+
+  uint32_t count = 0;
+  ddsrt_chh_enum_unsafe (chh, check ? chhtest_check : chhtest_count, &count);
+  printf ("nkeys in hash %"PRIu32"\n", count);
+  if (check)
+  {
+    for (uint32_t i = 0; i < nkeys; i++)
+    {
+      if (keyset[i] != UINT32_MAX)
+      {
+        CU_ASSERT_FATAL (count > 0);
+        count--;
+      }
+    }
+    CU_ASSERT_FATAL (count == 0);
+  }
+
+  ddsrt_chh_free (chh);
+  ddsrt_free (keyset);
+
+  while (gclist)
+  {
+    struct gcelem *elem = gclist;
+    gclist = gclist->next;
+    ddsrt_free (elem);
+  }
 }
