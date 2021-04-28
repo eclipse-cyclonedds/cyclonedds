57cf9490faa39b53a3e16e47c1118b107993c99c
Jeroen Koekkoek <jeroen@koekkoek.nl>
2021-04-28 16:32:11 +0200

    Add IDL_PRINT and IDL_PRINTA for C++ generator
    Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

---
+++ src/idl/include/idl/print.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright(c) 2021 ADLINK Technology Limited and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+#ifndef IDL_PRINT_H
+#define IDL_PRINT_H
+
+#include <stdio.h>
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#if defined(_MSC_VER)
+# include <malloc.h>
+#elif defined(__GNUC__) || (defined(__clang__) && __clang_major__ >= 2)
+# if !defined(__FreeBSD__)
+#   include <alloca.h>
+# endif
+#elif defined(__SUNPROC_C) || defined(__SUNPRO_CC)
+# include <alloca.h>
+#endif
+
+#include "idl/export.h"
+
+typedef int(*idl_print_t)(char *, size_t, const void *, void *);
+
+/** @private */
+IDL_EXPORT int idl_printa_arguments__(
+  char **strp, idl_print_t print, const void *object, void *user_data);
+
+/** @private */
+IDL_EXPORT size_t idl_printa_size__(void);
+
+/** @private */
+IDL_EXPORT char **idl_printa_strp__(void);
+
+/** @private */
+IDL_EXPORT int idl_printa__(void);
+
+#define IDL_COMMA__() ,
+#define IDL_SHIFT__(shift_, ...) shift_
+#define IDL_PICK__(drop1_, drop2_, pick_, ...) pick_
+#define IDL_EVALUATE__(eval_) eval_ /* required for MSVC */
+#define IDL_EXPAND__(...) IDL_EXPAND_AGAIN__(__VA_ARGS__)
+#define IDL_EXPAND_AGAIN__(...) IDL_EVALUATE__(IDL_PICK__(__VA_ARGS__, ))
+#define IDL_MAYBE__(...) \
+  IDL_EXPAND__(IDL_COMMA__ IDL_SHIFT__(__VA_ARGS__, ) (), NULL, __VA_ARGS__)
+
+#if _MSC_VER
+# define IDL_ALLOCA__(size_) (__pragma (warning(suppress: 6255)) alloca(size_))
+#else
+# define IDL_ALLOCA__(size_) (alloca(size_))
+#endif
+
+#define IDL_PRINTA__(strp_, print_, object_, ...) \
+  ((idl_printa_arguments__((strp_), (print_), (object_), IDL_MAYBE__(__VA_ARGS__)) >= 0) \
+   ? ((*(idl_printa_strp__()) = IDL_ALLOCA__(idl_printa_size__())), \
+       idl_printa__()) \
+   : (-1))
+
+#define IDL_PRINTA(strp_, print_, ...) \
+  IDL_PRINTA__((strp_), (print_), __VA_ARGS__, )
+
+IDL_EXPORT int idl_print__(
+  char **strp, idl_print_t print, const void *object, void *user_data);
+
+#define IDL_PRINT__(strp_, print_, object_, ...) \
+  idl_print__((strp_), (print_), (object_), IDL_MAYBE__(__VA_ARGS__))
+
+#define IDL_PRINT(strp_, print_, ...) \
+  IDL_PRINT__(strp_, print_, __VA_ARGS__, )
+
+#endif /* IDL_PRINT_H */
---
+++ src/idl/src/print.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright(c) 2021 ADLINK Technology Limited and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+#include "idl/print.h"
+
+#if defined(_MSC_VER)
+# define idl_thread_local __declspec(thread)
+#elif defined(__GNUC__) || (defined(__clang__) && __clang_major__ >= 2)
+# define idl_thread_local __thread
+#elif defined(__SUNPROC_C) || defined(__SUNPRO_CC)
+# define idl_thread_local __thread
+#else
+# error "Thread-local storage is not supported"
+#endif
+
+struct printa {
+  idl_print_t print;
+  const void *object;
+  void *user_data;
+  size_t size;
+  char *str, **strp;
+};
+
+static idl_thread_local struct printa printa;
+
+int idl_printa_arguments__(
+  char **strp, idl_print_t print, const void *object, void *user_data)
+{
+  int cnt;
+  char buf[1];
+
+  assert(strp);
+  assert(print);
+  assert(object);
+
+  if ((cnt = print(buf, sizeof(buf), object, user_data)) < 0)
+    return cnt;
+
+  printa.print = print;
+  printa.object = object;
+  printa.user_data = user_data;
+  printa.size = (size_t)cnt + 1;
+  printa.strp = strp;
+  printa.str = NULL;
+
+  return cnt;
+}
+
+size_t idl_printa_size__(void)
+{
+  assert(!printa.str);
+  return printa.size;
+}
+
+char **idl_printa_strp__(void)
+{
+  return &printa.str;
+}
+
+int idl_printa__(void)
+{
+  int cnt;
+
+  assert(printa.size);
+  assert(printa.str);
+  assert(printa.strp);
+
+  cnt = printa.print(
+    printa.str, printa.size, printa.object, printa.user_data);
+  if (cnt >= 0)
+    *printa.strp = printa.str;
+  printa.print = 0;
+  printa.object = NULL;
+  printa.user_data = NULL;
+  printa.size = 0;
+  printa.str = NULL;
+  printa.strp = NULL;
+  return cnt;
+}
+
+int idl_print__(
+  char **strp, idl_print_t print, const void *object, void *user_data)
+{
+  int cnt, len;
+  char buf[1], *str = NULL;
+
+  if ((len = print(buf, sizeof(buf), object, user_data)) < 0)
+    return len;
+  if (!(str = malloc((size_t)len + 1)))
+    return -1;
+  if ((cnt = print(str, (size_t)len + 1, object, user_data)) >= 0)
+    *strp = str;
+  else
+    free(str);
+  return cnt;
+}
