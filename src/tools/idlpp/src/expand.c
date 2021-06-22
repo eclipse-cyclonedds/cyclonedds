eclipse-cyclonedds/cyclonedds

commit dfa8a6e491395e940cb5e46d651f099950bff1a1

Fix warnings reported by Visual Studio analyzer in mcpp
Signed-off-by: Jeroen Koekkoek <jeroen@koekkoek.nl>

src/tools/idlpp/src/expand.c
@@ -526,6 +526,7 @@ static char *   chk_magic_balance(
 #if 0
                 mac_loc[ mac] = buf_p - 2;
 #endif
+                MSC_PRAGMA("warning(suppress: 6385)")
                 memcpy( mac_id[ mac], buf_p, MAC_S_LEN - 2);
             }
             mac++;
@@ -533,7 +534,9 @@ static char *   chk_magic_balance(
             break;
         case MAC_ARG_START  :
             if (option_flags.v) {
+                MSC_PRAGMA("warning(suppress: 6386)")
                 arg_loc[ arg] = buf_p - 2;
+                MSC_PRAGMA("warning(suppress: 6385)")
                 memcpy( arg_id[ arg], buf_p, ARG_S_LEN - 2);
             }
             arg++;
@@ -789,6 +792,7 @@ static char *   replace(
         diag_macro( CERROR, macbuf_overflow, defp->name, 0L, catbuf, defp
                 , NULL);
         if (nargs >= 0) {
+            assert( arglist);
             if (! enable_trace_macro)
                 /* arglist[0] is needed for macro infs  */
                 free( arglist[ 0]);
@@ -804,6 +808,7 @@ static char *   replace(
         dump_string( "prescan exit", catbuf);
     }
 
+    assert( arglist);
     if (nargs > 0) {    /* Function-like macro with any argument    */
         expbuf = xmalloc( (size_t) (NMACWORK + IDMAX));
         if (mcpp_debug & EXPAND) {
@@ -2650,6 +2655,8 @@ static int  get_an_arg(
     MAGIC_SEQ   mgc_seq;        /* Magic seqs and spaces succeeding an arg  */
     size_t  len;
 
+    memset( &mgc_seq, 0, sizeof (MAGIC_SEQ));
+
     if (trace_macro) {
         trace_arg = m_num && infile->fp;
         if (m_num) {
@@ -2679,7 +2686,6 @@ static int  get_an_arg(
                 argp += len;
             }
         }
-        memset( &mgc_seq, 0, sizeof (MAGIC_SEQ));
     }
 
     while (1) {
@@ -2863,6 +2869,7 @@ static int  squeeze_ws(
     FILE *  fp = infile->fp;
     int     end_of_file = (out && endf) ? FALSE : TRUE;
 
+    assert(! mgc_seq || out);
     while (((char_type[ c = get_ch()] & SPA) && (! standard
                 || (mcpp_mode == POST_STD && file == infile)
                 || (mcpp_mode == STD

src/tools/idlpp/src/configed.H.in
@@ -268,8 +268,16 @@ something so go through the trouble of defining it!
 
 #if HOST_COMPILER == GNUC
 #define MCPP_ATTRIBUTE(...) __attribute__(__VA_ARGS__)
+#define MCPP_ATTRIBUTE_NORETURN() __attribute__((noreturn))
+#define MSC_PRAGMA(...) /* empty */
+#elif HOST_COMPILER == MSC
+#define MCPP_ATTRIBUTE(...) /* empty */
+#define MCPP_ATTRIBUTE_NORETURN() __declspec(noreturn)
+#define MSC_PRAGMA(...) _Pragma(__VA_ARGS__)
 #else
 #define MCPP_ATTRIBUTE(...) /* empty */
+#define MCPP_ATTRIBUTE_NORETURN() /* empty */
+#define MSC_PRAGMA(...) /* empty */
 #endif
 
 /*

src/tools/idlpp/src/directive.c
@@ -705,6 +705,7 @@ DEFBUF *    do_define(
     size_t  def_start, def_end;     /* Column of macro definition   */
 
     def_start = def_end = 0;
+    memset(repl_list, 0, sizeof(repl_list));
     repl_base = repl_list;
     repl_end = & repl_list[ NMACWORK];
     c = skip_ws();
@@ -1616,6 +1617,7 @@ void    dump_a_def(
                 }
             }
 #if COMPILER == GNUC
+            assert( !(dp->nargs & VA_ARGS) || numargs > 0);
             if ((dp->nargs & VA_ARGS)
                     && memcmp( parms[ numargs - 1].name, "...", 3) != 0) {
                 appendix = "...";   /* Append ... so as to become 'args...' */

src/tools/idlpp/src/internal.H
@@ -486,9 +486,10 @@ extern char *   (xrealloc)( char * ptr, size_t size);
                 /* Reallocate memory or die     */
 extern LINE_COL *   get_src_location( LINE_COL * p_line_col);
                 /* Get location on source file  */
+
+MCPP_ATTRIBUTE_NORETURN()
 extern void     cfatal( const char * format, const char * arg1, long arg2
-        , const char * arg3)
-        MCPP_ATTRIBUTE((noreturn));
+        , const char * arg3);
                 /* Print a fatal error and exit */
 extern void     cerror( const char * format, const char * arg1, long arg2
         , const char * arg3);

src/tools/idlpp/src/support.c
@@ -161,9 +161,11 @@ void    mcpp_use_mem_buffers(
     use_mem_buffers = tf ? TRUE : FALSE;
 
     for (i = 0; i < NUM_OUTDEST; ++i) {
-        if (mem_buffers[ i].buffer)
+        if (mem_buffers[ i].buffer) {
             /* Free previously allocated memory buffer  */
+            MSC_PRAGMA("warning(suppress: 6001)")
             free( mem_buffers[ i].buffer);
+        }
         if (use_mem_buffers) {
             /* Output to memory buffers instead of files    */
             mem_buffers[ i].buffer = NULL;
@@ -1473,6 +1475,7 @@ void    expanding(
         clear_exp_mac();
         exp_mac_ind++;
     }
+    assert(exp_mac_ind < EXP_MAC_IND_MAX - 1);
     expanding_macro[ exp_mac_ind].name = name;
     expanding_macro[ exp_mac_ind].to_be_freed = to_be_freed;
 }

src/tools/idlpp/src/system.c
@@ -460,7 +460,7 @@ void    do_options(
     /* Get current directory for -I option and #pragma once */
     assert( cwd_sz < INT_MAX);
     cwd = getcwd( cur_work_dir, cwd_sz - 1);
-    assert( cwd == cur_work_dir);
+    assert( cwd && cwd == cur_work_dir);
 #if SYS_FAMILY == SYS_WIN
     bsl2sl( cwd);
 #endif
@@ -697,7 +697,7 @@ plus:
 
         case 'g':
             assert( mcpp_optarg != NULL);
-            if (!isdigit( *mcpp_optarg)
+            if (!isdigit( (unsigned char)*mcpp_optarg)
                     && str_eq( argv[ mcpp_optind - 2], "-g"))
                 /* Neither '-g 0' nor '-ggdb' -- No argument    */
                 mcpp_optind--;
@@ -753,7 +753,7 @@ plus:
 
         case 'h':
             assert( mcpp_optarg != NULL);
-            if (*(mcpp_optarg + 1) == EOS && isdigit( *mcpp_optarg))
+            if (*(mcpp_optarg + 1) == EOS && isdigit( (unsigned char)*mcpp_optarg))
                 /* a digit  */
                 look_and_install( "__STDC_HOSTED__", DEF_NOARGS_PREDEF, null
                         , mcpp_optarg);
@@ -776,7 +776,7 @@ plus:
                 unset_sys_dirs = TRUE;
                         /* Unset pre-specified include directories  */
 #endif
-            } else if (*(mcpp_optarg + 1) == EOS && isdigit( *mcpp_optarg)
+            } else if (*(mcpp_optarg + 1) == EOS && isdigit( (unsigned char)*mcpp_optarg)
                     && (i = *mcpp_optarg - '0') != 0
                     && (i & ~(CURRENT | SOURCE)) == 0) {
                 search_rule = i;            /* -I1, -I2 or -I3      */
@@ -1008,12 +1008,12 @@ plus:
             if (integrated_cpp) {
                 if (*mcpp_optarg == '-')            /* No argument  */
                     mcpp_optind--;
-                else if ((isdigit( *mcpp_optarg) && *mcpp_optarg != '0')
+                else if ((isdigit( (unsigned char)*mcpp_optarg) && *mcpp_optarg != '0')
                         || *mcpp_optarg == 's' || *mcpp_optarg == 'z')
                                             /* -O1, -O2 -Os, -Oz    */
                     look_and_install( "__OPTIMIZE__", DEF_NOARGS_PREDEF, null
                             , "1");
-                else if (! isdigit( *mcpp_optarg))
+                else if (! isdigit( (unsigned char)*mcpp_optarg))
                     usage( opt);
                 /* Else -O0: ignore */
             } else {
@@ -1241,7 +1241,7 @@ Version:
                                         /* Single-line diagnostic   */
             }
 #endif
-            if (isdigit( *mcpp_optarg)) {
+            if (isdigit( (unsigned char)*mcpp_optarg)) {
                 warn_level |= parse_warn_level( mcpp_optarg, opt);
                 if (warn_level > 31 || warn_level < 0)
                     usage( opt);
@@ -1711,9 +1711,9 @@ static int  parse_warn_level(
     while( *cp != EOS) {
         while( *cp == ' ')
             cp++;                           /* Skip spaces          */
-        if (! isdigit( *cp))
+        if (! isdigit( (unsigned char)*cp))
             break;                          /* Error    */
-        while (isdigit( *cp)) {
+        while (isdigit( (unsigned char)*cp)) {
             i *= 10;
             i += (*cp++ - '0');
         }
@@ -3058,7 +3058,7 @@ static char *   md_init(
  * Initialize output file and target.
  */
 {
-    char    prefix[ PATHMAX];
+    char    prefix[ PATHMAX] = { '\0' };
     char *  cp = NULL;
     size_t  len;
     char *  out_p;
@@ -3077,11 +3077,13 @@ static char *   md_init(
         memcpy( prefix, target, len);
         cp = prefix + len;
         *cp++ = '.';
+        *cp   = '\0';
     }
 
     if (! mkdep_fp) {   /* Unless already opened by -MF, -MD, -MMD options  */
         if (mkdep & MD_FILE) {
-            strcpy( cp, "d");
+            *cp++ = 'd';
+            *cp   = '\0';
             mkdep_fp = fopen( prefix, "w");
         } else {
             mkdep_fp = fp_out;  /* Output dependency line to normal output  */
@@ -4712,6 +4714,7 @@ static void do_preprocessed( void)
                 || (memcmp( --comment, "/* ", 3) != 0)
                 || ((colon = strrchr( comment, ':')) == NULL))
             cfatal( corrupted, NULL, 0L, NULL);
+        MSC_PRAGMA("warning(suppress: 28182 28183)")
         src_line = (size_t)atol( colon + 1);  /* Pseudo line number   */
         *colon = EOS;
         dir = comment + 3;
@@ -4993,8 +4996,10 @@ void    clear_filelist( void)
     for (incp = incdir; incp < incend; incp++)
         free( (void *) *incp);
     free( (void *) incdir);
-    for (namep = fnamelist; namep < fname_end; namep++)
+    for (namep = fnamelist; namep < fname_end; namep++) {
+        MSC_PRAGMA("warning(suppress: 6001)")
         free( (void *) namep->name);
+    }
     free( (void *) fnamelist);
     if (standard)
         free( (void *) once_list);

commit cae3db2b782f0216089eb45c594e59e8fa5599b2

Make __STDC__ work in IDLC by moving an assertion
The incorrectly placed assertion was introduced in dfa8a6e49; it is only
an assert so doesn't affect a release build.

Signed-off-by: Erik Boasson <eb@ilities.com>

src/tools/idlpp/src/expand.c
@@ -807,8 +807,8 @@ static char *   replace(
         dump_string( "prescan exit", catbuf);
     }
 
-    assert( arglist);
     if (nargs > 0) {    /* Function-like macro with any argument    */
+        assert( arglist);
         expbuf = xmalloc( (size_t) (NMACWORK + IDMAX));
         if (mcpp_debug & EXPAND) {
             mcpp_fprintf( DBG, "(%s)", defp->name);
@@ -827,9 +827,11 @@ static char *   replace(
             dump_string( "substitute exit", expbuf);
         }
     } else {                                /* Object-like macro or */
-        if (nargs == 0 && ! enable_trace_macro)
+        if (nargs == 0 && ! enable_trace_macro) {
                             /* Function-like macro with no argument */
+            assert( arglist);
             free( arglist[ 0]);
+        }
         free( arglist);
         out_p = expbuf = catbuf;
     }