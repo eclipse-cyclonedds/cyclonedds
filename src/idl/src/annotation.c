50ea91fe9ae763122701450327e0b349770e844f
Dennis Potman <dennis.potman@adlinktech.com>
2021-09-23 10:10:03 +0200

    Fix parameter type check in lib idl annotate_position
    Signed-off-by: Dennis Potman <dennis.potman@adlinktech.com>

--- src/idl/src/annotation.c
+++ src/idl/src/annotation.c
@@ -653,15 +653,12 @@ annotate_position(
   idl_annotation_appl_t *annotation_appl,
   idl_node_t *node)
 {
-#if !defined(NDEBUG)
-  static const idl_mask_t mask = IDL_LITERAL|IDL_USHORT;
-#endif
   idl_literal_t *literal;
 
   assert(annotation_appl);
   assert(annotation_appl->parameters);
   literal = (idl_literal_t *)annotation_appl->parameters->const_expr;
-  assert((idl_mask(literal) & mask) == mask);
+  assert(idl_type(literal) == IDL_USHORT);
 
   if (idl_is_bit_value(node)) {
     idl_bit_value_t *bit_value = (idl_bit_value_t *)node;
