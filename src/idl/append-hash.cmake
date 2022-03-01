81540881c404aa5cb4f294e142062d2a6e101d43
Thijs Miedema <mail@tmiedema.com>
2022-03-01 15:14:47 +0100

    Turn bison into an optional dependency (#1155)
    * Turn bison into an optional dependency
    
    * Create pre-commit hooks to make commit-time checks for bison/version sync
    
    * Append hash after running bison
    
    Signed-off-by: Erik Boasson <eb@ilities.com>
    
    * Ensure again removal of absolute paths in tree
    
    * Only run bison if parser.y changed
    
    Signed-off-by: Erik Boasson <eb@ilities.com>
    
    * Remove checked-in hook, check hash in parser.c/h instead.
    
    Co-authored-by: Erik Boasson <eb@ilities.com>

---
+++ src/idl/append-hash.cmake
@@ -0,0 +1,25 @@
+message("creating hash and writing to disk")
+
+# Hashing routine, remove \r to not get confused on windows if git changes the line endings.
+file(READ "${CMAKE_CURRENT_LIST_DIR}/src/parser.y" _parser_y_data)
+string(REPLACE "\r" "" _parser_y_data "${_parser_y_data}")
+string(SHA1 _parser_y_hash "${_parser_y_data}")
+
+# Store hash
+file(WRITE "${CMAKE_CURRENT_LIST_DIR}/src/parser.y.hash" "${_parser_y_hash}")
+
+# Write back cleaned and hash-appended parser.{c,h}
+string(REGEX REPLACE "[-+:/\\=()]" "_" _path_to_tok "${CMAKE_CURRENT_SOURCE_DIR}")
+string(TOUPPER "${_path_to_tok}" _path_to_tok)
+
+file(READ "${CMAKE_CURRENT_SOURCE_DIR}/parser.h" _parser_h_data)
+string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "" _parser_h_data "${_parser_h_data}")
+string(REPLACE "${_path_to_tok}" "" _parser_h_data "${_parser_h_data}")
+string(APPEND _parser_h_data "\n\n/* generated from parser.y[${_parser_y_hash}]*/\n")
+file(WRITE "${CMAKE_CURRENT_LIST_DIR}/src/parser.h" "${_parser_h_data}")
+
+file(READ "${CMAKE_CURRENT_SOURCE_DIR}/parser.c" _parser_c_data)
+string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "" _parser_c_data "${_parser_c_data}")
+string(REPLACE "${_path_to_tok}" "" _parser_c_data "${_parser_c_data}")
+string(APPEND _parser_c_data "\n\n/* generated from parser.y[${_parser_y_hash}]*/\n")
+file(WRITE "${CMAKE_CURRENT_LIST_DIR}/src/parser.c" "${_parser_c_data}")
