e706fd7da6b48781167913c9925374ce1f56bc1f
Thijs Miedema <mail@tmiedema.com>
2022-07-26 10:18:18 +0200

    Make MUSL build

--- src/idl/src/string.c
+++ src/idl/src/string.c
@@ -36,6 +36,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#if !defined(__USE_GNU) || !defined(__APPLE__) || !defined(__FreeBSD__)
+  #define __MUSL__
+#endif
+
 #if defined _WIN32
 # include <locale.h>
 typedef _locale_t locale_t;
@@ -268,7 +272,9 @@ unsigned long long idl_strtoull(const char *str, char **endptr, int base)
 {
   assert(str);
   assert(base >= 0 && base <= 36);
-#if _WIN32
+#ifdef __MUSL__
+  return strtoull(str, endptr, base);
+#elif _WIN32
 #if __GNUC__
   return strtoull(str, endptr, base);
 #else
