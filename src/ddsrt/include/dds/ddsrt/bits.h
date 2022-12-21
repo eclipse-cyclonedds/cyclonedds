6848810f4e403bda3d48c91e99ad4efe63269c82
Erik Boasson <eb@ilities.com>
2022-12-21 08:17:38 +0100

    Add find-first-set operation to ddsrt
    Signed-off-by: Erik Boasson <eb@ilities.com>

---
+++ src/ddsrt/include/dds/ddsrt/bits.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright(c) 2022 ZettaScale Technology and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+#ifndef DDSRT_BITS_H
+#define DDSRT_BITS_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "dds/export.h"
+#include "dds/ddsrt/static_assert.h"
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+/** \brief Find first set (POSIX style): returns index of least significant bit set in input
+
+    @param[in] x input, may be 0
+    @return position of least significant bit set in x (LSB is 1, MSB is 32), returns 0 if x == 0
+ */
+DDS_INLINE_EXPORT inline uint32_t ddsrt_ffs32u (uint32_t x)
+{
+  // Compiler support checks based on https://en.wikipedia.org/wiki/Find_first_set
+#if defined __clang__ && __clang_major__ >= 5
+  DDSRT_STATIC_ASSERT (sizeof (int) == sizeof (uint32_t));
+  return (uint32_t) __builtin_ffs ((int) x);
+#elif defined __GNUC__ && (__GNUC__ > 3 || __GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+  DDSRT_STATIC_ASSERT (sizeof (int) == sizeof (uint32_t));
+  return (uint32_t) __builtin_ffs ((int) x);
+#elif defined _MSC_VER && _MSC_VER >= 1400 // Visual Studio 2005
+  DDSRT_STATIC_ASSERT (sizeof (unsigned long) == sizeof (uint32_t));
+  unsigned long index;
+  return _BitScanForward (&index, x) ? (index + 1) : 0;
+#else
+  if (x == 0)
+    return 0;
+  uint32_t n = 1;
+  if ((x & 0x0000FFFF) == 0) { n += 16; x >>= 16; };
+  if ((x & 0x000000FF) == 0) { n +=  8; x >>=  8; };
+  if ((x & 0x0000000F) == 0) { n +=  4; x >>=  4; };
+  if ((x & 0x00000003) == 0) { n +=  2; x >>=  2; };
+  if ((x & 0x00000001) == 0) { n +=  1; };
+  return n;
+#endif
+}
+
+#if defined (__cplusplus)
+}
+#endif
+
+#endif /* DDSRT_BITS_H */
+
---
+++ src/ddsrt/tests/bits.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright(c) 2022 ZettaScale Technology and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+#include <stdio.h>
+#include "CUnit/Test.h"
+
+#include "dds/ddsrt/cdtors.h"
+#include "dds/ddsrt/bits.h"
+#include "dds/ddsrt/random.h"
+
+CU_Init(ddsrt_bits)
+{
+  ddsrt_init();
+  return 0;
+}
+
+CU_Clean(ddsrt_bits)
+{
+  ddsrt_fini();
+  return 0;
+}
+
+CU_Test(ddsrt_bits, ffs32u)
+{
+  // trivial cases: 0 and just 1 bit set in each possible position
+  CU_ASSERT (ddsrt_ffs32u (0) == 0);
+  int onebit_ok = 0;
+  for (uint32_t i = 0; i < 32; i++)
+    onebit_ok += ddsrt_ffs32u ((uint32_t)1 << i) == i + 1;
+  CU_ASSERT (onebit_ok == 32);
+
+  // all combinations of two bits
+  int twobit_ok = 0;
+  for (uint32_t i = 0; i < 31; i++)
+    for (uint32_t j = i+1; j < 32; j++)
+      twobit_ok += ddsrt_ffs32u (((uint32_t)1 << i) | ((uint32_t)1 << j)) == i + 1;
+  CU_ASSERT (twobit_ok == 32*31/2);
+
+  // random junk above the least significant bit set
+  int junk_ok = 0;
+  for (uint32_t i = 0; i < 31; i++)
+    for (uint32_t j = 0; j < 1000; j++)
+      junk_ok += ddsrt_ffs32u (((uint32_t)1 << i) | (ddsrt_random () << (i+1))) == i + 1;
+  CU_ASSERT (junk_ok == 31 * 1000);
+}
