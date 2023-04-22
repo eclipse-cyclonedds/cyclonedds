847b839de4988a3c8d69371e4e7e292c9c3add9c
Troy Karan Harrison <troy.harrison@zettascale.tech>
2023-04-22 21:34:52 +0200

    [CP-277] Catch last two violations of MISRA 3.1 in copyright blurbs

--- src/idl/src/parser.h
+++ src/idl/src/parser.h
@@ -1,22 +1,22 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+// A Bison parser, made by GNU Bison 3.5.1.
 
-/* Bison interface for Yacc-like parsers in C
-
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
-   Inc.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+// Bison interface for Yacc-like parsers in C
+//
+// Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+// Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
