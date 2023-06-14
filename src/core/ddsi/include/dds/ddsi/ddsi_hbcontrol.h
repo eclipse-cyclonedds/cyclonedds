3d9dd272b423ef67e18100733033e3839a6cb507
Erik Boasson <eb@ilities.com>
2023-06-14 11:52:12 +0200

    Document struct ddsi_hbcontrol
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/ddsi/include/dds/ddsi/ddsi_hbcontrol.h
+++ src/core/ddsi/include/dds/ddsi/ddsi_hbcontrol.h
@@ -18,13 +18,22 @@
 extern "C" {
 #endif
 
+/// @brief State information used in deciding when/what kind of a heartbeat to send (per writer)
+///
+/// Heartbeats inform readers of the range of sequence numbers available from the writer and serve the dual
+/// purpose of (1) allowing the reader to detect message loss, and (2) allowing the reader to request a retransmit.
+///
+/// Because readers are not allowed to request a retransmit unless they received a heartbeat, we make an
+/// effort to a embed heartbeat in an outgoing RTPS message (~ a packet) if a preceding RTPS message
+/// also contained data from the writer. Its sole purpose is to allow it to request a retransmit.
 struct ddsi_hbcontrol {
-  ddsrt_mtime_t t_of_last_write;
-  ddsrt_mtime_t t_of_last_hb;
-  ddsrt_mtime_t t_of_last_ackhb;
-  ddsrt_mtime_t tsched;
-  uint32_t hbs_since_last_write;
-  uint32_t last_packetid;
+  ddsrt_mtime_t t_of_last_write; ///< Time of most recent write
+  ddsrt_mtime_t t_of_last_hb;    ///< Time of last heartbeat sent
+  ddsrt_mtime_t t_of_last_ackhb; ///< Time of last heartbeat sent that requires a response
+  ddsrt_mtime_t tsched;          ///< Time at which next asynchronous heartbeat is scheduled
+  uint32_t hbs_since_last_write; ///< Number of heartbeats sent since last write
+  uint32_t last_packetid;        ///< Last RTPS message id containing a heartbeat from this writer
+};
 };
 
 #if defined (__cplusplus)
