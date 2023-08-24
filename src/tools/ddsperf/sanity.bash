88242f64f61963900f1a3c1f1930849a50a1bb76
Erik Boasson <eb@ilities.com>
2023-08-24 17:33:12 +0200

    Move ddsperf sanity check to its own pipeline step
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/tools/ddsperf/sanity.bash
+++ src/tools/ddsperf/sanity.bash
@@ -1,7 +1,9 @@
 exitcode=0
 # RSS/samples/roundtrip numbers are based on experimentation on Travis
-bin/ddsperf -L -D20 -n10 -Qminmatch:2 -Qrss:4 -Qsamples:300000 -Qroundtrips:3000 sub ping & ddsperf_pids=$!
-bin/ddsperf -L -D20 -n10 -Qminmatch:2 -Qrss:4 pub 100Hz burst 1000 & ddsperf_pids="$ddsperf_pids $!"
+d=bin
+[ -d bin/${BUILD_TYPE} ] && d=bin/${BUILD_TYPE}
+$d/ddsperf -L -D20 -n10 -Qminmatch:2 -Qrss:4 -Qsamples:300000 -Qroundtrips:3000 sub ping & ddsperf_pids=$!
+$d/ddsperf -L -D20 -n10 -Qminmatch:2 -Qrss:4 pub 100Hz burst 1000 & ddsperf_pids="$ddsperf_pids $!"
 sleep 21
 for pid in $ddsperf_pids ; do
     if kill -0 $pid 2>/dev/null ; then
