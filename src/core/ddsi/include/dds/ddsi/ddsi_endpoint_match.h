d6684dc96e7ef092869688358753e49e33e9bbc2
Erik Boasson <eb@ilities.com>
2023-10-11 17:17:31 +0200

    Fix get_matched_publication_data on builtin topic
    This crashed because the built-in topics are published by a "local
    orphan writer" that is not contained in a participant, causing
    dds_get_matched_publication_data to crash on dereferencing a null
    pointer.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/ddsi/include/dds/ddsi/ddsi_endpoint_match.h
+++ src/core/ddsi/include/dds/ddsi/ddsi_endpoint_match.h
@@ -32,11 +32,33 @@ dds_return_t ddsi_writer_get_matched_subscriptions (struct ddsi_writer *wr, uint
 /** @component endpoint_matching */
 dds_return_t ddsi_reader_get_matched_publications (struct ddsi_reader *rd, uint64_t *wrs, size_t nwrs);
 
-/** @component endpoint_matching */
-bool ddsi_writer_find_matched_reader (struct ddsi_writer *wr, uint64_t ih, struct ddsi_entity_common **rdc, struct dds_qos **rdqos, struct ddsi_entity_common **ppc);
+/** @brief Lookup the instance handle of a matching reader and return it, it's qos and participant
+ * @component endpoint_matching
+ *
+ * @note Thread state must be "awake", returned pointers are aliases; lifetime ends on leaving the "awake" state; contents may not be modified.
+ *
+ * @param[in] wr writer for which to lookup ih in the setting of matching readers
+ * @param[in] ih instance handle of a reader
+ * @param[out] rdc pointer to the common entity data of the reader
+ * @param[out] rdqos pointer to the QoS of the reader
+ * @param[out] ppc participant, may be null for an orphan reader
+ * @return true iff ih corresponds to a matched reader, output parameters are undefined if not
+ */
+bool ddsi_writer_find_matched_reader (struct ddsi_writer *wr, uint64_t ih, const struct ddsi_entity_common **rdc, const struct dds_qos **rdqos, const struct ddsi_entity_common **ppc);
 
-/** @component endpoint_matching */
-bool ddsi_reader_find_matched_writer (struct ddsi_reader *rd, uint64_t ih, struct ddsi_entity_common **wrc, struct dds_qos **wrqos, struct ddsi_entity_common **ppc);
+/** @brief Lookup the instance handle of a matching writer and return it, it's qos and participant
+ * @component endpoint_matching
+ *
+ * @note Thread state must be "awake", returned pointers are aliases; lifetime ends on leaving the "awake" state; contents may not be modified.
+ *
+ * @param[in] rd reader for which to lookup ih in the setting of matching writers
+ * @param[in] ih instance handle of a writer
+ * @param[out] wrc pointer to the common entity data of the writer
+ * @param[out] wrqos pointer to the QoS of the writer
+ * @param[out] ppc participant, may be null for an orphan writer
+ * @return true iff ih corresponds to a matched writer, output parameters are undefined if not
+ */
+bool ddsi_reader_find_matched_writer (struct ddsi_reader *rd, uint64_t ih, const struct ddsi_entity_common **wrc, const struct dds_qos **wrqos, const struct ddsi_entity_common **ppc);
 
 #if defined (__cplusplus)
 }
