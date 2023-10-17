513c1d7e6083eb69cf844bf275126410965f5ea3
TheFixer <thefixer@iteazz.com>
2023-10-17 10:25:01 +0200

    Use similar idl files for durable client as for durable server
    Signed-off-by: TheFixer <thefixer@iteazz.com>

--- src/durability/include/dds/durability/dds_durability.h
+++ src/durability/include/dds/durability/dds_durability.h
@@ -27,7 +27,7 @@ typedef int (*plugin_finalize)(void *context);
 
 dds_return_t dds_durability_init (const dds_domainid_t domain, struct ddsi_domaingv *gv);
 dds_return_t dds_durability_fini (void);
-void dds_durability_new_local_reader (struct dds_reader *reader, struct dds_rhc *rhc);
+dds_return_t dds_durability_new_local_reader (dds_entity_t reader, struct dds_rhc *rhc);
 dds_return_t dds_durability_new_local_writer (dds_entity_t writer);
 dds_return_t dds_durability_wait_for_quorum (dds_entity_t writer);
 
