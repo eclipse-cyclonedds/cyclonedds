32f3139e9df711323a129df5968d4575451af542
Erik Boasson <eb@ilities.com>
2024-03-20 12:29:03 +0100

    GCC analyzer dislikes non-null arg as loop var
    This despite the variable getting reassigned by a potentially null pointer. Using a
    separete variable solves the problem and is arguably better style.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/ddsi/src/ddsi_serdata_cdr.c
+++ src/core/ddsi/src/ddsi_serdata_cdr.c
@@ -155,18 +155,17 @@ static struct ddsi_serdata_cdr *serdata_cdr_from_ser_common (const struct ddsi_s
   if (!is_valid_xcdr_id (d->hdr.identifier))
     goto err;
 
-  while (fragchain)
+  for (const struct ddsi_rdata *frag = fragchain; frag != NULL; frag = frag->nextfrag)
   {
-    assert (fragchain->min <= off);
-    assert (fragchain->maxp1 <= size);
-    if (fragchain->maxp1 > off)
+    assert (frag->min <= off);
+    assert (frag->maxp1 <= size);
+    if (frag->maxp1 > off)
     {
       /* only copy if this fragment adds data */
-      const unsigned char *payload = DDSI_RMSG_PAYLOADOFF (fragchain->rmsg, DDSI_RDATA_PAYLOAD_OFF (fragchain));
-      serdata_cdr_append_blob (&d, fragchain->maxp1 - off, payload + off - fragchain->min);
-      off = fragchain->maxp1;
+      const unsigned char *payload = DDSI_RMSG_PAYLOADOFF (frag->rmsg, DDSI_RDATA_PAYLOAD_OFF (frag));
+      serdata_cdr_append_blob (&d, frag->maxp1 - off, payload + off - frag->min);
+      off = frag->maxp1;
     }
-    fragchain = fragchain->nextfrag;
   }
 
   const bool needs_bswap = !DDSI_RTPS_CDR_ENC_IS_NATIVE (d->hdr.identifier);
