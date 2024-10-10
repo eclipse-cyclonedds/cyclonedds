d5386501d54ae77d8cc709edd9258d27a9e21d7a
Erik Boasson <eb@ilities.com>
2024-10-10 17:11:28 +0200

    Should not modify the fuzzer input
    This modification started with normalizing bools and is the presumed cause for a flakey
    fuzzer in OSS fuzz.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- fuzz/fuzz_sample_deser/fuzz_sample_deser.c
+++ fuzz/fuzz_sample_deser/fuzz_sample_deser.c
@@ -1,5 +1,6 @@
 #include <ddsi__serdata_cdr.h>
 #include <dds/ddsrt/heap.h>
+#include <dds/ddsrt/string.h>
 #include <string.h>
 
 #include "fuzz_samples.h"
@@ -21,14 +22,16 @@ int LLVMFuzzerTestOneInput(void *data, size_t size)
         const struct dds_topic_descriptor *topic = fixed_types[i];
         struct dds_cdrstream_desc desc;
         topic_to_descriptor(&desc, topic);
-        if (dds_stream_normalize(data, (uint32_t) size, false, DDSI_RTPS_CDR_ENC_VERSION_2, &desc, false, &actual_size)) {
+        void *data_copy = ddsrt_memdup (data, size);
+        if (dds_stream_normalize(data_copy, (uint32_t) size, false, DDSI_RTPS_CDR_ENC_VERSION_2, &desc, false, &actual_size)) {
             void *sample = ddsrt_calloc(1, desc.size);
             dds_istream_t is;
-            dds_istream_init(&is, (uint32_t) size, data, DDSI_RTPS_CDR_ENC_VERSION_2);
+            dds_istream_init(&is, (uint32_t) size, data_copy, DDSI_RTPS_CDR_ENC_VERSION_2);
             dds_stream_read_sample(&is, sample, &dds_cdrstream_default_allocator, &desc);
             dds_stream_free_sample(sample, &dds_cdrstream_default_allocator, desc.ops.ops);
             ddsrt_free(sample);
         }
+        ddsrt_free (data_copy);
         dds_cdrstream_desc_fini(&desc, &dds_cdrstream_default_allocator);
     }
     return 0;
