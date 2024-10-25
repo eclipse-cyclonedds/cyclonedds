cec4361a54dc2fa3a4896a697501f661acdc8523
Erik Boasson <eb@ilities.com>
2024-10-25 10:17:05 +0200

    Add missing atomic uintptr/uint64 functions
    That is, missing in the sense that an uint32-bit version was present but
    an uint64 and/or uintptr version was not.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/ddsrt/include/dds/ddsrt/atomics/arm.h
+++ src/ddsrt/include/dds/ddsrt/atomics/arm.h
@@ -91,13 +91,13 @@ inline void ddsrt_atomic_stvoidp (volatile ddsrt_atomic_voidp_t *x, void *v) { d
 
 /**
  * @brief Compare and swap a 32-bit unsigned integer atomically
- * 
+ *
  * Assigns des to x iff x == exp
  *
  * @param[in,out] x Pointer to the variable whose value to swap
  * @param[in] exp the value to test x for
  * @param[in] des the value to assign to the variable
- * 
+ *
  * @return int meant to be interpreted as bool (swapped: 1; not swapped: 0)
  */
 inline int ddsrt_atomic_cas32 (volatile ddsrt_atomic_uint32_t *x, uint32_t exp, uint32_t des) {
@@ -119,13 +119,13 @@ inline int ddsrt_atomic_cas32 (volatile ddsrt_atomic_uint32_t *x, uint32_t exp, 
 
 /**
  * @brief Compare and swap an uintptr atomically
- * 
+ *
  * Assigns des to x iff x == exp
  *
  * @param[in,out] x Pointer to the variable whose value to swap
  * @param[in] exp the value to test x for
  * @param[in] des the value to assign to the variable
- * 
+ *
  * @return int meant to be interpreted as bool (swapped: 1; not swapped: 0)
  */
 inline int ddsrt_atomic_casptr (volatile ddsrt_atomic_uintptr_t *x, uintptr_t exp, uintptr_t des) {
@@ -134,13 +134,13 @@ inline int ddsrt_atomic_casptr (volatile ddsrt_atomic_uintptr_t *x, uintptr_t ex
 
 /**
  * @brief Compare and swap a void pointer atomically
- * 
+ *
  * Assigns des to x iff x == exp
  *
  * @param[in,out] x Pointer to the variable whose value to swap
  * @param[in] exp the value to test x for
  * @param[in] des the value to assign to the variable
- * 
+ *
  * @return int meant to be interpreted as bool (swapped: 1; not swapped: 0)
  */
 inline int ddsrt_atomic_casvoidp (volatile ddsrt_atomic_voidp_t *x, void *exp, void *des) {
@@ -171,6 +171,18 @@ inline uint32_t ddsrt_atomic_add32_nv (volatile ddsrt_atomic_uint32_t *x, uint32
 }
 
 /**
+ * @brief Increment (by given amount) an uintptr atomically and return its new value
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ * @param[in] v The value to increment by
+ *
+ * @return The new value of the incremented variable
+ */
+inline uintptr_t ddsrt_atomic_addptr_nv (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+  return ddsrt_atomic_add32_nv ((volatile ddsrt_atomic_uint32_t *) x, v);
+}
+
+/**
  * @brief Increment (by given amount) a 32-bit unsigned integer atomically
  *
  * @param[in,out] x Pointer to the variable to increment
@@ -178,6 +190,16 @@ inline uint32_t ddsrt_atomic_add32_nv (volatile ddsrt_atomic_uint32_t *x, uint32
  */
 inline void ddsrt_atomic_add32 (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     (void) ddsrt_atomic_add32_nv (x, v);
+}
+
+/**
+ * @brief Increment (by given amount) an uintptr atomically
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ * @param[in] v The value to increment by
+ */
+inline void ddsrt_atomic_addptr (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    (void) ddsrt_atomic_addptr_nv (x, v);
 }
 
 /**
@@ -192,6 +214,18 @@ inline uint32_t ddsrt_atomic_add32_ov (volatile ddsrt_atomic_uint32_t *x, uint32
     return ddsrt_atomic_add32_nv (x, v) - v;
 }
 
+/**
+ * @brief Increment (by given amount) an uintptr atomically and return its old value
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ * @param[in] v The value to increment by
+ *
+ * @return The old value of the incremented variable
+ */
+inline uint32_t ddsrt_atomic_addptr_ov (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    return ddsrt_atomic_addptr_nv (x, v) - v;
+}
+
 /* SUB */
 
 /**
@@ -199,11 +233,47 @@ inline uint32_t ddsrt_atomic_add32_ov (volatile ddsrt_atomic_uint32_t *x, uint32
  *
  * @param[in,out] x Pointer to the variable to decrement
  * @param[in] v The value to decrement by
- * 
+ *
  * @return The new value of the decremented variable
  */
 inline uint32_t ddsrt_atomic_sub32_nv (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     return ddsrt_atomic_add32_nv (x, -v);
+}
+
+/**
+ * @brief Decrement (by given amount) an uintptr atomically and return its new value
+ *
+ * @param[in,out] x Pointer to the variable to decrement
+ * @param[in] v The value to decrement by
+ *
+ * @return The new value of the decremented variable
+ */
+inline uint32_t ddsrt_atomic_subptr_nv (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    return ddsrt_atomic_addptr_nv (x, -v);
+}
+
+/**
+ * @brief Decrement (by given amount) a 32-bit unsigned integer atomically and return its old value
+ *
+ * @param[in,out] x Pointer to the variable to decrement
+ * @param[in] v The value to decrement by
+ *
+ * @return The new value of the decremented variable
+ */
+inline uint32_t ddsrt_atomic_sub32_ov (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
+    return ddsrt_atomic_add32_ov (x, -v);
+}
+
+/**
+ * @brief Decrement (by given amount) an uintptr atomically and return its new value
+ *
+ * @param[in,out] x Pointer to the variable to decrement
+ * @param[in] v The value to decrement by
+ *
+ * @return The new value of the decremented variable
+ */
+inline uint32_t ddsrt_atomic_subptr_ov (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    return ddsrt_atomic_addptr_ov (x, -v);
 }
 
 /**
@@ -216,6 +286,16 @@ inline void ddsrt_atomic_sub32 (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     ddsrt_atomic_add32 (x, -v);
 }
 
+/**
+ * @brief Decrement (by given amount) an uintptr atomically
+ *
+ * @param[in,out] x Pointer to the variable to decrement
+ * @param[in] v The value to decrement by
+ */
+inline void ddsrt_atomic_subptr (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    ddsrt_atomic_addptr (x, -v);
+}
+
 /* INC */
 
 /**
@@ -226,7 +306,18 @@ inline void ddsrt_atomic_sub32 (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
  * @return The old value of the incremented variable
  */
 inline uint32_t ddsrt_atomic_inc32_ov (volatile ddsrt_atomic_uint32_t *x) {
-    return ddsrt_atomic_add32_nv (x, 1) - 1;
+    return ddsrt_atomic_add32_ov (x, 1);
+}
+
+/**
+ * @brief Increment (by one) an uintptr atomically and return its old value
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ *
+ * @return The old value of the incremented variable
+ */
+inline uintptr_t ddsrt_atomic_incptr_ov (volatile ddsrt_atomic_uintptr_t *x) {
+    return ddsrt_atomic_addptr_ov (x, 1);
 }
 
 /**
@@ -241,12 +332,32 @@ inline uint32_t ddsrt_atomic_inc32_nv (volatile ddsrt_atomic_uint32_t *x) {
 }
 
 /**
+ * @brief Increment (by one) an uintptr atomically and return its new value
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ *
+ * @return The new value of the incremented variable
+ */
+inline uintptr_t ddsrt_atomic_incptr_nv (volatile ddsrt_atomic_uintptr_t *x) {
+    return ddsrt_atomic_addptr_nv (x, 1);
+}
+
+/**
  * @brief Increment (by one) a 32-bit unsigned integer atomically
  *
  * @param[in,out] x Pointer to the variable to increment
  */
 inline void ddsrt_atomic_inc32 (volatile ddsrt_atomic_uint32_t *x) {
     (void) ddsrt_atomic_inc32_nv (x);
+}
+
+/**
+ * @brief Increment (by one) an uintptr integer atomically
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ */
+inline void ddsrt_atomic_incptr (volatile ddsrt_atomic_uintptr_t *x) {
+    (void) ddsrt_atomic_incptr_nv (x);
 }
 
 /* DEC */
@@ -259,7 +370,18 @@ inline void ddsrt_atomic_inc32 (volatile ddsrt_atomic_uint32_t *x) {
  * @return The old value of the decremented variable
  */
 inline uint32_t ddsrt_atomic_dec32_ov (volatile ddsrt_atomic_uint32_t *x) {
-    return ddsrt_atomic_sub32_nv (x, 1) + 1;
+    return ddsrt_atomic_sub32_ov (x, 1);
+}
+
+/**
+ * @brief Decrement (by one) a 32-bit unsigned integer atomically and return its old value
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ *
+ * @return The old value of the decremented variable
+ */
+inline uintptr_t ddsrt_atomic_decptr_ov (volatile ddsrt_atomic_uintptr_t *x) {
+    return ddsrt_atomic_subptr_ov (x, 1);
 }
 
 /**
@@ -274,12 +396,32 @@ inline uint32_t ddsrt_atomic_dec32_nv (volatile ddsrt_atomic_uint32_t *x) {
 }
 
 /**
+ * @brief Decrement (by one) an uintptr atomically and return its new value
+ *
+ * @param[in,out] x Pointer to the variable to increment
+ *
+ * @return The new value of the decremented variable
+ */
+inline uintptr_t ddsrt_atomic_decptr_nv (volatile ddsrt_atomic_uintptr_t *x) {
+    return ddsrt_atomic_subptr_nv (x, 1);
+}
+
+/**
  * @brief Decrement (by one) a 32-bit unsigned integer atomically
  *
  * @param[in,out] x Pointer to the variable to decrement
  */
 inline void ddsrt_atomic_dec32 (volatile ddsrt_atomic_uint32_t *x) {
     (void) ddsrt_atomic_dec32_nv (x);
+}
+
+/**
+ * @brief Decrement (by one) an uintptr atomically
+ *
+ * @param[in,out] x Pointer to the variable to decrement
+ */
+inline void ddsrt_atomic_decptr (volatile ddsrt_atomic_uintptr_t *x) {
+    (void) ddsrt_atomic_decptr_nv (x);
 }
 
 /* AND */
@@ -289,7 +431,7 @@ inline void ddsrt_atomic_dec32 (volatile ddsrt_atomic_uint32_t *x) {
  *
  * @param[in,out] x Pointer to the variable to AND
  * @param[in] v The value to AND with the variable
- * 
+ *
  * @return The old value of the ANDed variable
  */
 inline uint32_t ddsrt_atomic_and32_ov (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
@@ -299,16 +441,44 @@ inline uint32_t ddsrt_atomic_and32_ov (volatile ddsrt_atomic_uint32_t *x, uint32
 }
 
 /**
+ * @brief Bitwise AND an uintptr atomically and return the old value
+ *
+ * @param[in,out] x Pointer to the variable to AND
+ * @param[in] v The value to AND with the variable
+ *
+ * @return The old value of the ANDed variable
+ */
+inline uintptr_t ddsrt_atomic_andptr_ov (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    uintptr_t oldval, newval;
+    do { oldval = x->v; newval = oldval & v; } while (!ddsrt_atomic_casptr (x, oldval, newval));
+    return oldval;
+}
+
+/**
  * @brief Bitwise AND a 32-bit unsigned integer atomically and return the new value
  *
  * @param[in,out] x Pointer to the variable to AND
  * @param[in] v The value to AND with the variable
- * 
+ *
  * @return The new value of the ANDed variable
  */
 inline uint32_t ddsrt_atomic_and32_nv (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     uint32_t oldval, newval;
     do { oldval = x->v; newval = oldval & v; } while (!ddsrt_atomic_cas32 (x, oldval, newval));
+    return newval;
+}
+
+/**
+ * @brief Bitwise AND an uintptr atomically and return the new value
+ *
+ * @param[in,out] x Pointer to the variable to AND
+ * @param[in] v The value to AND with the variable
+ *
+ * @return The new value of the ANDed variable
+ */
+inline uintptr_t ddsrt_atomic_andptr_nv (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    uintptr_t oldval, newval;
+    do { oldval = x->v; newval = oldval & v; } while (!ddsrt_atomic_casptr (x, oldval, newval));
     return newval;
 }
 
@@ -322,6 +492,16 @@ inline void ddsrt_atomic_and32 (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     (void) ddsrt_atomic_and32_nv (x, v);
 }
 
+/**
+ * @brief Bitwise AND an uintptr atomically
+ *
+ * @param[in,out] x Pointer to the variable to AND
+ * @param[in] v The value to AND with the variable
+ */
+inline void ddsrt_atomic_andptr (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    (void) ddsrt_atomic_andptr_nv (x, v);
+}
+
 /* OR */
 
 /**
@@ -329,7 +509,7 @@ inline void ddsrt_atomic_and32 (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
  *
  * @param[in,out] x Pointer to the variable to OR
  * @param[in] v The value to OR with the variable
- * 
+ *
  * @return The old value of the ORed variable
  */
 inline uint32_t ddsrt_atomic_or32_ov (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
@@ -339,16 +519,44 @@ inline uint32_t ddsrt_atomic_or32_ov (volatile ddsrt_atomic_uint32_t *x, uint32_
 }
 
 /**
+ * @brief Bitwise OR an uintptr atomically and return the old value
+ *
+ * @param[in,out] x Pointer to the variable to OR
+ * @param[in] v The value to OR with the variable
+ *
+ * @return The old value of the ORed variable
+ */
+inline uintptr_t ddsrt_atomic_orptr_ov (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    uintptr_t oldval, newval;
+    do { oldval = x->v; newval = oldval | v; } while (!ddsrt_atomic_casptr (x, oldval, newval));
+    return oldval;
+}
+
+/**
  * @brief Bitwise OR a 32-bit unsigned integer atomically and return the new value
  *
  * @param[in,out] x Pointer to the variable to OR
  * @param[in] v The value to OR with the variable
- * 
+ *
  * @return The new value of the ORed variable
  */
 inline uint32_t ddsrt_atomic_or32_nv (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     uint32_t oldval, newval;
     do { oldval = x->v; newval = oldval | v; } while (!ddsrt_atomic_cas32 (x, oldval, newval));
+    return newval;
+}
+
+/**
+ * @brief Bitwise OR an uintptr atomically and return the new value
+ *
+ * @param[in,out] x Pointer to the variable to OR
+ * @param[in] v The value to OR with the variable
+ *
+ * @return The new value of the ORed variable
+ */
+inline uintptr_t ddsrt_atomic_orptr_nv (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    uintptr_t oldval, newval;
+    do { oldval = x->v; newval = oldval | v; } while (!ddsrt_atomic_casptr (x, oldval, newval));
     return newval;
 }
 
@@ -360,6 +568,16 @@ inline uint32_t ddsrt_atomic_or32_nv (volatile ddsrt_atomic_uint32_t *x, uint32_
  */
 inline void ddsrt_atomic_or32 (volatile ddsrt_atomic_uint32_t *x, uint32_t v) {
     (void) ddsrt_atomic_or32_nv (x, v);
+}
+
+/**
+ * @brief Bitwise OR an uintptr atomically
+ *
+ * @param[in,out] x Pointer to the variable to OR
+ * @param[in] v The value to OR with the variable
+ */
+inline void ddsrt_atomic_orptr (volatile ddsrt_atomic_uintptr_t *x, uintptr_t v) {
+    (void) ddsrt_atomic_orptr_nv (x, v);
 }
 
 /* FENCES */
