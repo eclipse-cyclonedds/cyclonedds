5af5f2766f6ce10959eb54c0f71b01ffe6985a55
Erik Boasson <eb@ilities.com>
2024-12-03 12:56:42 +0100

    Add missing "or is_wstring" by introducing "is_xstring" to test for either kind
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/idl/include/idl/tree.h
+++ src/idl/include/idl/tree.h
@@ -537,6 +537,9 @@ IDL_EXPORT bool idl_is_bounded_string(const void *node);
 IDL_EXPORT bool idl_is_wstring(const void *node);
 IDL_EXPORT bool idl_is_unbounded_wstring(const void *node);
 IDL_EXPORT bool idl_is_bounded_wstring(const void *node);
+IDL_EXPORT bool idl_is_xstring(const void *node); // string || wstring
+IDL_EXPORT bool idl_is_unbounded_xstring(const void *node);
+IDL_EXPORT bool idl_is_bounded_xstring(const void *node);
 IDL_EXPORT bool idl_is_constr_type(const void *node);
 IDL_EXPORT bool idl_is_struct(const void *node);
 IDL_EXPORT bool idl_is_empty(const void *node);
