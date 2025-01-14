2b3d8bbbbf5a9c5cf18965041f17e3bda12d6cfb
Erik Boasson <eb@ilities.com>
2025-01-14 13:39:37 +0100

    Fix dds_psmx_get_config_option_value
    This should follow the same syntax rules as the config string checker.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/xtests/symbol_export/symbol_export.c
+++ src/core/xtests/symbol_export/symbol_export.c
@@ -540,6 +540,7 @@ int main (int argc, char **argv)
   dds_psmx_topic_init_generic (ptr, ptr2, ptr3, ptr4, ptr, 0);
   dds_psmx_topic_cleanup_generic (ptr);
   dds_psmx_supported_features (ptr);
+  dds_psmx_get_config_option_value (ptr, ptr2);
 
   // dds_loaned_sample.h
   dds_loaned_sample_ref (ptr);
