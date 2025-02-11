0fb031cd9ca4b69543340fd06e4bf18093f87534
Erik Boasson <eb@ilities.com>
2025-02-11 13:40:19 +0100

    Disable some memory bounds checks in gcc for idlpp
    Suppressing the warnings with _Pragma locally doesn't work reliably and new trouble ensues
    with every change gcc version.  (On Linux on x64 anyway, I don't know why Linux on aarch64
    doesn't have the same problems.)
    
    So a blanket disable of treating the problematic warnings as errors for idlpp it is.
    
    In the hope that later gcc versions get rid of the false positives, this limits it to a
    range of gcc versions for which the problem has been observed.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/tools/idlpp/src/expand.c
+++ src/tools/idlpp/src/expand.c
@@ -479,11 +479,6 @@ static char *   print_macro_arg(
  * This routine is only called from above print_macro_inf().
  */
 {
-#if defined(__GNUC__) && ((__GNUC__ * 100) + __GNUC_MINOR__) >= 1300 && ((__GNUC__ * 100) + __GNUC_MINOR__) < 1400
-_Pragma("GCC diagnostic push")
-_Pragma("GCC diagnostic ignored \"-Wformat-overflow\"")
-_Pragma("GCC diagnostic ignored \"-Warray-bounds\"")
-#endif
     LOCATION *  loc = m_inf->loc_args + argn;
 
     out += sprintf( out, "/*%s%s:%d-%d", real_arg ? "!" : (start ? "<" : "")
@@ -500,9 +495,6 @@ _Pragma("GCC diagnostic ignored \"-Warray-bounds\"")
     out = stpcpy( out, "*/");
 
     return out;
-#if defined(__GNUC__) && ((__GNUC__ * 100) + __GNUC_MINOR__) >= 1300 && ((__GNUC__ * 100) + __GNUC_MINOR__) < 1400
-_Pragma("GCC diagnostic pop")
-#endif
 }
 
 static char *   chk_magic_balance(
