0cf894933b845a4ff13b9c2b4a8cccf1e5cab9d6
Erik Boasson <eb@ilities.com>
2025-08-11 15:37:25 +0200

    Add Iceoryx2 plugin
    The Iceoryx2 plugin seems to work well enough, but there are still some vague
    issues (possibly misunderstandings) that it tries to workaround, and there are still some
    open questions on QoS settings and how best to map DDS traffic to Iceoryx2 services.
    
    Co-authored-by: Martijn Reicher <martijn.reicher@zettascale.tech>
    Signed-off-by: Erik Boasson <eb@ilities.com>

---
+++ src/psmx_iox/include/psmx_iox2_impl.h
@@ -0,0 +1,26 @@
+// Copyright(c) 2024 ZettaScale Technology and others
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+// v. 1.0 which is available at
+// http://www.eclipse.org/org/documents/edl-v10.php.
+//
+// SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+
+#include <stdbool.h>
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#include "dds/dds.h"
+#include "dds/ddsc/dds_loaned_sample.h"
+#include "dds/ddsc/dds_psmx.h"
+#include "psmx_iox2_export.h"
+
+DDS_PSMX_IOX2_EXPORT dds_return_t iox2_create_psmx (struct dds_psmx **psmx, dds_psmx_instance_id_t instance_id, const char *config);
+
+#if defined (__cplusplus)
+}
+#endif
