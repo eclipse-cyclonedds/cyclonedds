434ffb6acf0132b5affe4c61b42b7a0622b6c082
Erik Boasson <eb@ilities.com>
2025-09-04 14:21:57 +0200

    Simplify data_type_properties
    No real value is in knowing that a type (transitively) contains, e.g., an array. When such
    details are relevant, it is almost invariably necessary to know a lot more and the full
    type definition needs to be inspected.
    
    What is relevant is to know whether the type contains key fields or contains pointers (if
    not, it can be memcpy'd). What is also relevant is whether the type meets the restrictions
    of pre-XTypes DDS.
    
    This removes all the useless flags while retaining (source & binary) backwards
    compatibility, except for the presumed non-existent code that really rely on the bits that
    for presence of integers, bools, strings, &c.
    
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- src/core/ddsc/include/dds/ddsc/dds_data_type_properties.h
+++ src/core/ddsc/include/dds/ddsc/dds_data_type_properties.h
@@ -19,26 +19,35 @@ extern "C" {
 
 /**
  * @brief Flags that are used to indicate the types used in a data type
+ *
+ * Bits 0 .. 16 are kinda reserved for presence of a variety of types, but its really not worth the bother.
+ * The old names are left in for backwards source compatibility with other sertype implementations.
+ * Eventually they can be reused, if necessary.
  */
-#define DDS_DATA_TYPE_CONTAINS_UNION              (0x1ull << 0)
-#define DDS_DATA_TYPE_CONTAINS_BITMASK            (0x1ull << 1)
-#define DDS_DATA_TYPE_CONTAINS_ENUM               (0x1ull << 2)
-#define DDS_DATA_TYPE_CONTAINS_STRUCT             (0x1ull << 3)
-#define DDS_DATA_TYPE_CONTAINS_STRING             (0x1ull << 4)
-#define DDS_DATA_TYPE_CONTAINS_BSTRING            (0x1ull << 5)
-#define DDS_DATA_TYPE_CONTAINS_WSTRING            (0x1ull << 6)
-#define DDS_DATA_TYPE_CONTAINS_SEQUENCE           (0x1ull << 7)
-#define DDS_DATA_TYPE_CONTAINS_BSEQUENCE          (0x1ull << 8)
-#define DDS_DATA_TYPE_CONTAINS_ARRAY              (0x1ull << 9)
+#define DDS_DATA_TYPE_CONTAINS_UNION              (0ull)
+#define DDS_DATA_TYPE_CONTAINS_BITMASK            (0ull)
+#define DDS_DATA_TYPE_CONTAINS_ENUM               (0ull)
+#define DDS_DATA_TYPE_CONTAINS_STRUCT             (0ull)
+#define DDS_DATA_TYPE_CONTAINS_STRING             (0ull)
+#define DDS_DATA_TYPE_CONTAINS_BSTRING            (0ull)
+#define DDS_DATA_TYPE_CONTAINS_WSTRING            (0ull)
+#define DDS_DATA_TYPE_CONTAINS_SEQUENCE           (0ull)
+#define DDS_DATA_TYPE_CONTAINS_BSEQUENCE          (0ull)
+#define DDS_DATA_TYPE_CONTAINS_ARRAY              (0ull)
 #define DDS_DATA_TYPE_CONTAINS_OPTIONAL           (0x1ull << 10)
-#define DDS_DATA_TYPE_CONTAINS_EXTERNAL           (0x1ull << 11)
-#define DDS_DATA_TYPE_CONTAINS_KEY                (0x1ull << 12)
-#define DDS_DATA_TYPE_CONTAINS_BWSTRING           (0x1ull << 13)
-#define DDS_DATA_TYPE_CONTAINS_WCHAR              (0x1ull << 14)
+#define DDS_DATA_TYPE_CONTAINS_EXTERNAL           (0ull)
+#define DDS_DATA_TYPE_CONTAINS_BWSTRING           (0ull)
+#define DDS_DATA_TYPE_CONTAINS_WCHAR              (0ull)
 #define DDS_DATA_TYPE_CONTAINS_APPENDABLE         (0x1ull << 15)
 #define DDS_DATA_TYPE_CONTAINS_MUTABLE            (0x1ull << 16)
 
+// Bits 10, 15 and 16 used to be optional, appendable and mutable, respectively: and if some of these
+// were set, the default was XCDR2. Now we set only bit 10, but check for any of the 3 bits.
+#define DDS_DATA_TYPE_DEFAULTS_TO_XCDR2           (DDS_DATA_TYPE_CONTAINS_OPTIONAL)
+#define DDS_DATA_TYPE_DEFAULTS_TO_XCDR2_MASK      (DDS_DATA_TYPE_CONTAINS_OPTIONAL | DDS_DATA_TYPE_CONTAINS_APPENDABLE | DDS_DATA_TYPE_CONTAINS_MUTABLE)
+#define DDS_DATA_TYPE_CONTAINS_KEY                (0x1ull << 12)
 #define DDS_DATA_TYPE_IS_MEMCPY_SAFE              (0x1ull << 63)
+
 
 typedef uint64_t dds_data_type_properties_t;
 
