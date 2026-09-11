49622d94ceb36e28856954bdab17c2d1cb20206d
bing.li <bing.li@processmission.com>
2026-09-11 09:52:12 +0800

    ports/zephyr: finish the hello world example restructuring
    The other examples pair a hand-written <name>_main.c with the pub/sub
    translation units generated by copy_examples.sh, and that script has been
    generating helloworld_pub.c/helloworld_sub.c all along. The hello world
    example was left half-way: the CMakeLists does not list those generated
    sources, so helloworld_main.c still carries its own publisher and subscriber
    loops. List the generated sources and reduce the entry point to a dispatcher,
    like the other examples do.
    
    Signed-off-by: bing.li <bing.li@processmission.com>
    
    Change-Id: I779464ad964042359b6d51033ff8a75255490527

--- ports/zephyr/src/helloworld_main.c
+++ ports/zephyr/src/helloworld_main.c
@@ -1,11 +1,15 @@
-#include <zephyr/kernel.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <zephyr/kernel.h>
 #include <dds/dds.h>
+
 #include "HelloWorldData.h"
 #include "helloworld_config.h"
+
+int helloworld_pub(int argc, char **argv);
+int helloworld_sub(int argc, char **argv);
 
 /* cdds_xml_config[] is generated from config.xml by GENERATE_CDDS_CONF and holds
  * the NUL-terminated string "CYCLONEDDS_URI=<CycloneDDS>...</CycloneDDS>".
@@ -48,149 +52,14 @@ static void set_cyclonedds_uri(void)
     printf("Failed to set CYCLONEDDS_URI\n");
 }
 
-void helloworld_publisher()
-{
-  dds_entity_t participant;
-  dds_entity_t topic;
-  dds_entity_t writer;
-  dds_return_t rc;
-  HelloWorldData_Msg msg;
-  uint32_t status = 0;
-
-  /* Create a Participant. */
-  participant = dds_create_participant (1, NULL, NULL);
-  if (participant < 0)
-    DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
-
-  /* Create a Topic. */
-  topic = dds_create_topic (
-    participant, &HelloWorldData_Msg_desc, "HelloWorldData_Msg", NULL, NULL);
-  if (topic < 0)
-    DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
-
-  /* Create a Writer. */
-  writer = dds_create_writer (participant, topic, NULL, NULL);
-  if (writer < 0)
-    DDS_FATAL("dds_create_writer: %s\n", dds_strretcode(-writer));
-
-  printf("=== [Publisher]  Waiting for a reader to be discovered ...\n");
-  fflush (stdout);
-
-  rc = dds_set_status_mask(writer, DDS_PUBLICATION_MATCHED_STATUS);
-  if (rc != DDS_RETCODE_OK)
-    DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-rc));
-
-  while(!(status & DDS_PUBLICATION_MATCHED_STATUS))
-  {
-    rc = dds_get_status_changes (writer, &status);
-    if (rc != DDS_RETCODE_OK)
-      DDS_FATAL("dds_get_status_changes: %s\n", dds_strretcode(-rc));
-
-    /* Polling sleep. */
-    dds_sleepfor (DDS_MSECS (20));
-  }
-
-  /* Create a message to write. */
-  msg.userID = 1;
-  msg.message = "Hello World";
-
-  printf ("=== [Publisher]  Writing : ");
-  printf ("Message (%"PRId32", %s)\n", msg.userID, msg.message);
-  fflush (stdout);
-
-  rc = dds_write (writer, &msg);
-  if (rc != DDS_RETCODE_OK)
-    DDS_FATAL("dds_write: %s\n", dds_strretcode(-rc));
-
-  /* Deleting the participant will delete all its children recursively as well. */
-  rc = dds_delete (participant);
-  if (rc != DDS_RETCODE_OK)
-    DDS_FATAL("dds_delete: %s\n", dds_strretcode(-rc));
-}
-
-#define MAX_SAMPLES 1
-
-void helloworld_subscriber()
-{
-  dds_entity_t participant;
-  dds_entity_t topic;
-  dds_entity_t reader;
-  HelloWorldData_Msg *msg;
-  void *samples[MAX_SAMPLES];
-  dds_sample_info_t infos[MAX_SAMPLES];
-  dds_return_t rc;
-  dds_qos_t *qos;
-
-  /* Create a Participant. */
-  participant = dds_create_participant (1, NULL, NULL);
-  if (participant < 0)
-    DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
-
-  /* Create a Topic. */
-  topic = dds_create_topic (
-    participant, &HelloWorldData_Msg_desc, "HelloWorldData_Msg", NULL, NULL);
-  if (topic < 0)
-    DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
-
-  /* Create a reliable Reader. */
-  qos = dds_create_qos ();
-  dds_qset_reliability (qos, DDS_RELIABILITY_RELIABLE, DDS_SECS (10));
-
-  reader = dds_create_reader (participant, topic, qos, NULL);
-  if (reader < 0)
-    DDS_FATAL("dds_create_reader: %s\n", dds_strretcode(-reader));
-  dds_delete_qos(qos);
-
-  printf ("\n=== [Subscriber] Waiting for a sample ...\n");
-  fflush (stdout);
-
-  /* Initialize sample buffer, by pointing the void pointer within
-   * the buffer array to a valid sample memory location. */
-  samples[0] = HelloWorldData_Msg__alloc ();
-
-  /* Poll until data has been read. */
-  while (true)
-  {
-    /* Do the actual read.
-     * The return value contains the number of read samples. */
-    rc = dds_read (reader, samples, infos, MAX_SAMPLES, MAX_SAMPLES);
-    if (rc < 0)
-      DDS_FATAL("dds_read: %s\n", dds_strretcode(-rc));
-
-    /* Check if we read some data and it is valid. */
-    if ((rc > 0) && (infos[0].valid_data))
-    {
-      /* Print Message. */
-      msg = (HelloWorldData_Msg*) samples[0];
-      printf ("=== [Subscriber] Received : ");
-      printf ("Message (%"PRId32", %s)\n", msg->userID, msg->message);
-      fflush (stdout);
-      break;
-    }
-    else
-    {
-      /* Polling sleep. */
-      dds_sleepfor (DDS_MSECS (20));
-    }
-  }
-
-  /* Free the data location. */
-  HelloWorldData_Msg_free (samples[0], DDS_FREE_ALL);
-
-  /* Deleting the participant will delete all its children recursively as well. */
-  rc = dds_delete (participant);
-  if (rc != DDS_RETCODE_OK)
-    DDS_FATAL("dds_delete (participant): %s\n", dds_strretcode(-rc));
-}
-
-int main(void)
+int main(int argc, char **argv)
 {
     printf("CycloneDDS Hello World! %s\n", CONFIG_BOARD);
     set_cyclonedds_uri();
 #if BUILD_HELLOWORLD_PUB
-    helloworld_publisher();
+    helloworld_pub(argc, argv);
 #else
-    helloworld_subscriber();
+    helloworld_sub(argc, argv);
 #endif
     printf("Done\n");
     return 0;
